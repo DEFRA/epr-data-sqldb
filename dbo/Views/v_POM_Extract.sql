8f7c1ad20511009f645247cfc602e65c4b2f802f
Rakesh.Mamidala <Rakesh.Mamidala@defradev.onmicrosoft.com>
2024-02-26 16:37:27 +0000

    Changed objects for 278511& 278671

--- dbo/Views/v_POM_Extract.sql
+++ dbo/Views/v_POM_Extract.sql
@@ -1,33 +1,43 @@
-﻿CREATE VIEW [dbo].[v_POM_Extract] AS SELECT distinct
+﻿CREATE VIEW [dbo].[v_POM_Extract] AS SELECT DISTINCT
 dsf.FromOrganisation_Name [Org_Name]
 /*,Case 
     When dsf.[FromOrganisation_IsComplianceScheme] = 'True' then 'Compliance Scheme'
     else 'Producer'
 End [PCS_Or_Direct_Producer] 
 */
-,case when csname.name is not null then  'Compliance Scheme'
-else'Producer' end [PCS_Or_Direct_Producer]
+,CASE
+	WHEN csname.name is not null THEN  'Compliance Scheme'
+	ELSE 'Producer' END [PCS_Or_Direct_Producer]
  
 /*,case 
     when dsf.[ToOrganisation_IsComplianceScheme] = 'True' then dsf.[ToOrganisation_Name]
     when dsf.[FromOrganisation_IsComplianceScheme] = 'True' then dsf.[FromOrganisation_Name]
 else NULL
 end   [Compliance_Scheme]*/
+
 --,dsf.[ComplianceSchemes_Name] Compliance_Scheme
+
 --,case when csname.name is not null then csname.name else dsf.[ComplianceSchemes_Name] end Compliance_Scheme 278515
 ,csname.name as Compliance_Scheme
-,dsf.FromOrganisation_Type [Org_Type]
-,case 
-    when reg.[organisation_sub_type_code]  = 'LIC' then 'Licensor'
-    when reg.[organisation_sub_type_code]  = 'POB' then 'Pub operating business '
-    when reg.[organisation_sub_type_code]  = 'FRA' then 'Franchisor '
-    when reg.[organisation_sub_type_code]  = 'NAO' then 'Non-associated organisation'
-    when reg.[organisation_sub_type_code]  = 'HCY' then 'Holding company'
-    when reg.[organisation_sub_type_code]  = 'SUB' then 'Subsidiary'
-    when reg.[organisation_sub_type_code]  = 'LFR' then 'Licensee/Franchisee'
-    when reg.[organisation_sub_type_code]  = 'TEN' then 'Tenant'
-    when reg.[organisation_sub_type_code]  = 'OTH' then 'Others'
-else NULL end [Org_Sub_Type] 
+,dsf.FromOrganisation_Type   as [Org_Type]
+
+-- ********==========================================**********************
+-- The below code included in the "v_registration_latest" ********
+--,case 
+--    when reglatest.[Org_Sub_Type]  = 'LIC' then 'Licensor'
+--    when reglatest.[Org_Sub_Type]  = 'POB' then 'Pub operating business '
+--    when reglatest.[Org_Sub_Type]  = 'FRA' then 'Franchisor '
+--    when reglatest.[Org_Sub_Type]  = 'NAO' then 'Non-associated organisation'
+--    when reglatest.[Org_Sub_Type]  = 'HCY' then 'Holding company'
+--    when reglatest.[Org_Sub_Type]  = 'SUB' then 'Subsidiary'
+--    when reglatest.[Org_Sub_Type]  = 'LFR' then 'Licensee/Franchisee'
+--    when reglatest.[Org_Sub_Type]  = 'TEN' then 'Tenant'
+--    when reglatest.[Org_Sub_Type]  = 'OTH' then 'Others'
+--else NULL end [org_sub_type]
+-- ********==========================================**********************
+
+
+,reglatest.[Org_Sub_Type] --New column for org_sub_type
 ,p.[organisation_size]
 ,meta.created [Submission_Date]
 ,p.[submission_period]
@@ -54,30 +64,32 @@ else NULL end [Org_Sub_Type]
 ,case when meta.[SubmittedBy] is null then 'No Name Available'
  
 else meta.[SubmittedBy] end [SubmittedBy]
+
 /*,Case 
     When dsf.[FromOrganisation_IsComplianceScheme] = 'True' then 'Compliance Scheme'
     else 'Producer'
 End [PCS_Or_Direct_Producer_enrolled] 
 */
-,case when csname.name is not null then  csname.name
-else dsf.FromOrganisation_Name end submitter_org_name
+,CASE
+	WHEN csname.name is not null THEN  csname.name
+	ELSE dsf.FromOrganisation_Name END submitter_org_name
  
-,case when os.organisation_id is null then p.[organisation_id] 
-else os.organisation_id end submitter_org_id
+,CASE
+	WHEN os.organisation_id is null THEN p.[organisation_id] 
+	ELSE os.organisation_id END submitter_org_id
  
 --,dsf.ServiceRoles_Role
 --,dsf.[from_nation] FromNation
 --,dsf.[tonation] ToNation
+
 FROM [dbo].[v_Pom] p
-   join [dbo].[v_rpd_data_SECURITY_FIX] dsf
-on p.[organisation_id]  = dsf.[FromOrganisation_ReferenceNumber] 
-   join [dbo].[v_cosmos_file_metadata] meta
-on p.filename = meta.filename
---20231205
-left join ( select cosmos.filename, cs.name
-  from [dbo].[v_cosmos_file_metadata] cosmos
-  join  rpd.complianceschemes cs on cs.externalid = cosmos.[ComplianceSchemeId]
-  group by  cosmos.filename, cs.name) csname on csname.filename = p.filename
-left join [rpd].[CompanyDetails] reg on reg.[organisation_id] = p.[organisation_id]
-left join [v_POM_Operator_Submissions] os on os.filename = p.filename
-where p.[submission_period] is not null;
+
+	join [dbo].[v_rpd_data_SECURITY_FIX] dsf on p.[organisation_id]  = dsf.[FromOrganisation_ReferenceNumber]
+	left join [v_POM_Operator_Submissions] os on os.filename = p.filename
+	join [dbo].[v_cosmos_file_metadata] meta on p.filename = meta.filename
+
+	left join ( select cosmos.filename, cs.name from [dbo].[v_cosmos_file_metadata] cosmos join  rpd.complianceschemes cs on cs.externalid = cosmos.[ComplianceSchemeId]
+											group by  cosmos.filename, cs.name) csname on csname.filename = p.filename
+
+	left join [dbo].[v_registration_latest] reglatest on  reglatest.[organisation_id] = p.[organisation_id] and reglatest.[subsidiary_id] =p.[subsidiary_id]
+WHERE p.[submission_period] is not null;
