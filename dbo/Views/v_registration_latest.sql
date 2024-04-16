af6e3b8b404f3caf4a5045cfd475c78fad89fe88
rakeshmamidala221 <156447227+rakeshmamidala221@users.noreply.github.com>
2024-04-16 11:18:48 +0100

    for 347928 (#23)

--- dbo/Views/v_registration_latest.sql
+++ dbo/Views/v_registration_latest.sql
@@ -1,34 +1,36 @@
-﻿CREATE VIEW [dbo].[v_registration_latest] AS SELECT  distinct
-rv.organisation_id,
-rv.subsidiary_id,
-registration_type_code
-,case 
-    when rv.[organisation_sub_type_code]  = 'LIC' then 'Licensor'
-    when rv.[organisation_sub_type_code]  = 'POB' then 'Pub operating business '
-    when rv.[organisation_sub_type_code]  = 'FRA' then 'Franchisor '
-    when rv.[organisation_sub_type_code]  = 'NAO' then 'Non-associated organisation'
-    when rv.[organisation_sub_type_code]  = 'HCY' then 'Holding company'
-    when rv.[organisation_sub_type_code]  = 'SUB' then 'Subsidiary'
-    when rv.[organisation_sub_type_code]  = 'LFR' then 'Licensee/Franchisee'
-    when rv.[organisation_sub_type_code]  = 'TEN' then 'Tenant'
-    when rv.[organisation_sub_type_code]  = 'OTH' then 'Others'
-else NULL end [Org_Sub_Type], 
-cs.created
-FROM 
-       rpd.CompanyDetails rv
-        join v_cosmos_file_metadata cs on cs.filename = rv.[FileName]
-INNER JOIN 
-    (
-        SELECT 
-            organisation_id, 
-            subsidiary_id, 
-            MAX(cs.created) AS created
-        FROM 
-         rpd.CompanyDetails cd
-             join v_cosmos_file_metadata cs on cs.filename = cd.[FileName]
-        GROUP BY 
-            organisation_id, 
-            subsidiary_id
-    ) latest_files ON rv.organisation_id = latest_files.organisation_id 
-                    AND rv.subsidiary_id = latest_files.subsidiary_id
-                    AND cs.created = latest_files.created;
+﻿CREATE VIEW [dbo].[v_registration_latest] AS select *
+from
+(
+	SELECT  distinct
+	rv.organisation_id,
+	rv.subsidiary_id,
+	registration_type_code
+	,cs.created as SubmittedDateTime
+	,cs.filename
+	,case 
+		when rv.[organisation_sub_type_code]  = 'LIC' then 'Licensor'
+		when rv.[organisation_sub_type_code]  = 'POB' then 'Pub operating business '
+		when rv.[organisation_sub_type_code]  = 'FRA' then 'Franchisor '
+		when rv.[organisation_sub_type_code]  = 'NAO' then 'Non-associated organisation'
+		when rv.[organisation_sub_type_code]  = 'HCY' then 'Holding company'
+		when rv.[organisation_sub_type_code]  = 'SUB' then 'Subsidiary'
+		when rv.[organisation_sub_type_code]  = 'LFR' then 'Licensee/Franchisee'
+		when rv.[organisation_sub_type_code]  = 'TEN' then 'Tenant'
+		when rv.[organisation_sub_type_code]  = 'OTH' then 'Others'
+	else NULL end [Org_Sub_Type], 
+	rv.companies_house_number,
+	rv.organisation_name,
+	rv.Trading_Name,
+	rv.registered_addr_line1,
+	rv.registered_addr_line2,
+	rv.registered_city,
+	rv.registered_addr_country,
+	rv.registered_addr_county,
+	rv.registered_addr_postcode,
+	cs.created,
+	row_number() over(partition by organisation_id, subsidiary_id order by cs.created desc) as rn
+	FROM 
+		   rpd.CompanyDetails rv
+			join v_cosmos_file_metadata cs on cs.filename = rv.[FileName]
+)A
+where A.rn = 1;
