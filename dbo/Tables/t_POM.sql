399317d5b3559d1898908ff3aebaa0555c608656
roshan-shaikh <161720098+roshan-shaikh@users.noreply.github.com>
2024-06-12 17:38:49 +0100

    Feature/390978 (#28) (#29)
    * User Story 390978 Pom Extract Fix
    
    * Pritam's Changes : Bit to varchar for CD Flag Fields.

--- dbo/Tables/t_POM.sql
+++ dbo/Tables/t_POM.sql
@@ -4,6 +4,7 @@
     [organisation_size]           NVARCHAR (4000) NULL,
     [organisation_sub_type_code]  VARCHAR (1)     NOT NULL,
     [submission_period]           VARCHAR (34)    NULL,
+    [submission_period_tile]      NVARCHAR (4000) NULL,
     [packaging_activity]          VARCHAR (34)    NULL,
     [packaging_type]              VARCHAR (34)    NULL,
     [packaging_class]             VARCHAR (34)    NULL,
@@ -17,7 +18,12 @@
     [FileName]                    NVARCHAR (4000) NULL,
     [Quantity_kg_extrapolated]    FLOAT (53)      NULL,
     [Quantity_units_extrapolated] FLOAT (53)      NULL,
-    [relative_move]               VARCHAR (72)    NULL
+    [relative_move]               VARCHAR (72)    NULL,
+    [File_submitted_time]         DATETIME        NULL,
+    [Rank_over_time]              BIGINT          NULL,
+    [IsLatest]                    INT             NOT NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
+
 
