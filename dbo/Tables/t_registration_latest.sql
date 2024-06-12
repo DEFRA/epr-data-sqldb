6ff2d8cf09296a4d77bae35ad9d6f551bf9e4971
roshan-shaikh <161720098+roshan-shaikh@users.noreply.github.com>
2024-06-12 17:06:31 +0100

    Feature/390978 (#28)
    * User Story 390978 Pom Extract Fix
    
    * Pritam's Changes : Bit to varchar for CD Flag Fields.

--- dbo/Tables/t_registration_latest.sql
+++ dbo/Tables/t_registration_latest.sql
@@ -1,11 +1,25 @@
 ﻿CREATE TABLE [dbo].[t_registration_latest] (
-    [organisation_id]        INT             NULL,
-    [subsidiary_id]          NVARCHAR (4000) NULL,
-    [registration_type_code] NVARCHAR (4000) NULL,
-    [Org_Sub_Type]           VARCHAR (27)    NULL,
-    [created]                DATETIME        NULL
+    [organisation_id]          INT             NULL,
+    [subsidiary_id]            NVARCHAR (4000) NULL,
+    [registration_type_code]   NVARCHAR (4000) NULL,
+    [SubmittedDateTime]        DATETIME        NULL,
+    [filename]                 NVARCHAR (4000) NULL,
+    [Org_Sub_Type]             VARCHAR (27)    NULL,
+    [companies_house_number]   NVARCHAR (4000) NULL,
+    [organisation_name]        NVARCHAR (4000) NULL,
+    [Trading_Name]             NVARCHAR (4000) NULL,
+    [registered_addr_line1]    NVARCHAR (4000) NULL,
+    [registered_addr_line2]    NVARCHAR (4000) NULL,
+    [registered_city]          NVARCHAR (4000) NULL,
+    [registered_addr_country]  NVARCHAR (4000) NULL,
+    [registered_addr_county]   NVARCHAR (4000) NULL,
+    [registered_addr_postcode] NVARCHAR (4000) NULL,
+    [created]                  DATETIME        NULL,
+    [rn]                       BIGINT          NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
+
 
 
 
