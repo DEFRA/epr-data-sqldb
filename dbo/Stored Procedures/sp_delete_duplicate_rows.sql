ba3af59aef4e78368e2181821da1ead61c2c3d95
SamuelThomas2 <156189631+SamuelThomas2@users.noreply.github.com>
2024-07-08 15:31:31 +0100

    Delete dbo/Stored Procedures/sp_delete_duplicate_rows.sql

--- dbo/Stored Procedures/sp_delete_duplicate_rows.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROC [dbo].[sp_delete_duplicate_rows] AS
-
--- delete Duplicates From Cosmosdb
-IF OBJECT_ID('rpd.cosmos_file_metadata', 'u') IS NOT NULL 
-DELETE t1
-FROM rpd.cosmos_file_metadata t1
-JOIN rpd.cosmos_file_metadata t2
-  ON t1.filename = t2.filename
-  AND t1.load_ts < t2.load_ts;
