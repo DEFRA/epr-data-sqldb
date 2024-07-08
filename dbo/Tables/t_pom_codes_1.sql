b52c82998471cf3b75de0c99fe40bcd6e3ea4c6f
SamuelThomas2 <156189631+SamuelThomas2@users.noreply.github.com>
2024-07-08 15:36:24 +0100

    Feature/remove sql objects (#45)
    * Delete dbo/Tables/t_pom_codes.sql
    
    * Rename t_pom_codes_1.sql to t_pom_codes.sql
    
    * Delete dbo/Stored Procedures/sp_delete_duplicate_rows.sql
    
    * Rename sp_delete_duplicate_rows_1.sql to sp_delete_duplicate_rows.sql

--- dbo/Tables/t_pom_codes_1.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[t_pom_codes] (
-    [Code] VARCHAR (7)  NOT NULL,
-    [Type] VARCHAR (22) NOT NULL,
-    [Text] VARCHAR (34) NOT NULL
-)
-WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
-
-
-
