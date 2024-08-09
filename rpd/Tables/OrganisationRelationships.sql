b3b76bc4fce18be19c102c5fb13b29c5cc496685
HelenGardner26 <156445988+HelenGardner26@users.noreply.github.com>
2024-08-09 12:57:52 +0100

    Ticket 410948 adittional subsids (#70)
    Co-authored-by: Helen.Gardner <helen.gardner@defradev.onmicrosoft.com>

---
+++ rpd/Tables/OrganisationRelationships.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [rpd].[OrganisationRelationships] (
+    [Id]                             INT             NULL,
+    [FirstOrganisationId]            INT             NULL,
+    [SecondOrganisationId]           INT             NULL,
+    [OrganisationRelationshipTypeId] INT             NULL,
+    [RelationFromDate]               DATETIME2 (7)   NULL,
+    [RelationToDate]                 DATETIME2 (7)   NULL,
+    [RelationExpiryReason]           NVARCHAR (4000) NULL,
+    [CreatedOn]                      NVARCHAR (4000) NULL,
+    [LastUpdatedById]                INT             NULL,
+    [LastUpdatedOn]                  NVARCHAR (4000) NULL,
+    [LastUpdatedByOrganisationId]    INT             NULL,
+    [OrganisationRegistrationTypeId] INT             NULL,
+    [load_ts]                        DATETIME2 (7)   NULL
+)
+WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
