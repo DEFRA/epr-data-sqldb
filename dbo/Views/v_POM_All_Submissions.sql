ec0365966c2652351e8f42665bcedc882b5aee8b
HelenGardner26 <156445988+HelenGardner26@users.noreply.github.com>
2024-08-09 12:57:52 +0100

    Ticket 410948 adittional subsids (#70)
    Co-authored-by: Helen.Gardner <helen.gardner@defradev.onmicrosoft.com>

--- dbo/Views/v_POM_All_Submissions.sql
+++ dbo/Views/v_POM_All_Submissions.sql
@@ -1,159 +1,165 @@
-﻿CREATE VIEW [dbo].[v_POM_All_Submissions] AS With vPOM_AS As 
+﻿CREATE VIEW [dbo].[v_POM_All_Submissions] AS With vPOM_AS 
+As 
 (
 
-select A.*, d.Regulator_Status,	d.Regulator_User_Name,	d.Decision_Date ,	d.Regulator_Rejection_Comments
-/***************************************************************************************************
-History:
+	select A.*, d.Regulator_Status,	d.Regulator_User_Name,	d.Decision_Date ,	d.Regulator_Rejection_Comments
+	,so.SecondOrganisation_ReferenceNumber as SubsidiaryOrganisation_ReferenceNumber
+	/***************************************************************************************************
+	History:
 
-	Updated 2024-07-23: SN001: Display Org name with Org ID all the packaging reports.
-							Ticket 412287 for Release 5.0
+		Updated 2024-07-23: SN001: Display Org name with Org ID all the packaging reports.
+								Ticket 412287 for Release 5.0
 	
-	Updated 2024-07-08: [Initials]001: [Update text here]
+		Updated 2024-07-08: [Initials]001: [Update text here]
 
-*****************************************************************************************************/
-from
-(
-		SELECT [Org_Name]
-		,MemberName		= [Org_Name]  /**  SN001: Added  **/
-			  ,[PCS_Or_Direct_Producer]
-			  ,[Compliance_Scheme]
-			  ,[Org_Type]
-			  ,[Org_Sub_Type]
-			  ,[organisation_size]
-			  ,[Submission_Date]
-			  ,[submission_period]
-			  ,[organisation_id]
-			  ,[subsidiary_id]
-			  ,[CH_Number]
-			  ,[Nation_Of_Enrolment]
-			  ,[packaging_activity]
-			  ,[packaging_type]
-			  ,[packaging_class]
-			  ,[packaging_material]
-			  ,[packaging_sub_material]
-			  ,[from_nation]
-			  ,[to_nation]
-			  ,[quantity_kg]
-			  ,[quantity_unit]
-			  ,[Quantity_kg_extrapolated]
-			  ,[Quantity_units_extrapolated]
-			  ,[ToOrganisation_NationName]
-			  ,[Nation]
-			  ,[FromOrganisation_NationName]
-			  ,[FileName]
-			  ,[ServiceRoles_Role]
-			  ,[SubmittedBy]
-			  ,[filetype]
-			  ,[Users_Email]
-			  ,[Persons_Email]
-			  ,[metafile]
-			  ,[JOINFIELD]
-			  ,[relative_move]
-			  ,[TransferNation]
-			  ,[SubmtterEmail]
-			  ,[ServiceRoles_Name]
-			  ,[OriginalFileName]
-			  ,'Direct' data_type
-		FROM t_POM_Submissions direct
-		WHERE direct.FileName NOT IN ( SELECT DISTINCT operators.FileName 
-										FROM v_POM_Operator_Submissions operators )
+	*****************************************************************************************************/
+	from
+	(
+			SELECT [Org_Name]
+			,MemberName		= [Org_Name]  /**  SN001: Added  **/
+				  ,[PCS_Or_Direct_Producer]
+				  ,[Compliance_Scheme]
+				  ,[Org_Type]
+				  ,[Org_Sub_Type]
+				  ,[organisation_size]
+				  ,[Submission_Date]
+				  ,[submission_period]
+				  ,[organisation_id]
+				  ,[subsidiary_id]
+				  ,[CH_Number]
+				  ,[Nation_Of_Enrolment]
+				  ,[packaging_activity]
+				  ,[packaging_type]
+				  ,[packaging_class]
+				  ,[packaging_material]
+				  ,[packaging_sub_material]
+				  ,[from_nation]
+				  ,[to_nation]
+				  ,[quantity_kg]
+				  ,[quantity_unit]
+				  ,[Quantity_kg_extrapolated]
+				  ,[Quantity_units_extrapolated]
+				  ,[ToOrganisation_NationName]
+				  ,[Nation]
+				  ,[FromOrganisation_NationName]
+				  ,[FileName]
+				  ,[ServiceRoles_Role]
+				  ,[SubmittedBy]
+				  ,[filetype]
+				  ,[Users_Email]
+				  ,[Persons_Email]
+				  ,[metafile]
+				  ,[JOINFIELD]
+				  ,[relative_move]
+				  ,[TransferNation]
+				  ,[SubmtterEmail]
+				  ,[ServiceRoles_Name]
+				  ,[OriginalFileName]
+				  ,'Direct' data_type
+			FROM t_POM_Submissions direct
+			WHERE direct.FileName NOT IN ( SELECT DISTINCT operators.FileName 
+											FROM v_POM_Operator_Submissions operators )
  
-		UNION
-		--add in operator
-		SELECT 
-			   [Org_Name]
-			   ,MemberName		=  [Producer_Org_Name] /**  SN001: Added  **/
-			  ,[PCS_Or_Direct_Producer]
-			  ,[Compliance_Scheme]
-			  ,[Org_Type]
-			  ,[Org_Sub_Type]
-			  ,[organisation_size]
-			  ,[Submission_Date]
-			  ,[submission_period]
-			  ,[organisation_id]
-			  ,[subsidiary_id]
-			  ,[CH_Number]
-			  ,[Nation_Of_Enrolment]
-			  ,[packaging_activity]
-			  ,[packaging_type]
-			  ,[packaging_class]
-			  ,[packaging_material]
-			  ,[packaging_sub_material]
-			  ,[from_nation]
-			  ,[to_nation]
-			  ,[quantity_kg]
-			  ,[quantity_unit]
-			  ,[Quantity_kg_extrapolated]
-			  ,[Quantity_units_extrapolated]
-			  ,[ToOrganisation_NationName]
-			  ,[Nation]
-			  ,[FromOrganisation_NationName]
-			  ,[FileName]
-			  ,[ServiceRoles_Role]
-			  ,[SubmittedBy]
-			  ,[filetype]
-			  ,[Users_Email]
-			  ,[Persons_Email]
-			  ,[metafile]
-			  ,[JOINFIELD]
-			  ,[relative_move]
-			  ,[TransferNation]
-			  ,[SubmtterEmail]
-			  ,[ServiceRoles_Name]
-			  ,[OriginalFileName], 
-			  'Operator'
-		FROM v_POM_Operator_Submissions
+			UNION
+			--add in operator
+			SELECT 
+				   [Org_Name]
+				   ,MemberName		=  [Producer_Org_Name] /**  SN001: Added  **/
+				  ,[PCS_Or_Direct_Producer]
+				  ,[Compliance_Scheme]
+				  ,[Org_Type]
+				  ,[Org_Sub_Type]
+				  ,[organisation_size]
+				  ,[Submission_Date]
+				  ,[submission_period]
+				  ,[organisation_id]
+				  ,[subsidiary_id]
+				  ,[CH_Number]
+				  ,[Nation_Of_Enrolment]
+				  ,[packaging_activity]
+				  ,[packaging_type]
+				  ,[packaging_class]
+				  ,[packaging_material]
+				  ,[packaging_sub_material]
+				  ,[from_nation]
+				  ,[to_nation]
+				  ,[quantity_kg]
+				  ,[quantity_unit]
+				  ,[Quantity_kg_extrapolated]
+				  ,[Quantity_units_extrapolated]
+				  ,[ToOrganisation_NationName]
+				  ,[Nation]
+				  ,[FromOrganisation_NationName]
+				  ,[FileName]
+				  ,[ServiceRoles_Role]
+				  ,[SubmittedBy]
+				  ,[filetype]
+				  ,[Users_Email]
+				  ,[Persons_Email]
+				  ,[metafile]
+				  ,[JOINFIELD]
+				  ,[relative_move]
+				  ,[TransferNation]
+				  ,[SubmtterEmail]
+				  ,[ServiceRoles_Name]
+				  ,[OriginalFileName], 
+				  'Operator'
+			FROM v_POM_Operator_Submissions
  
-		UNION 
+			UNION 
 
-		SELECT
-			   [Org_Name]
-			   ,MemberName		= [Producer_Org_Name]  /**  SN001: Added  **/
-			  ,[PCS_Or_Direct_Producer]
-			  ,[Compliance_Scheme]
-			  ,[Org_Type]
-			  ,[Org_Sub_Type]
-			  ,[organisation_size]
-			  ,[Submission_Date]
-			  ,[submission_period]
-			        ,[organisation_id]  /**  SN001: Uncommented  **/
-			  --,[organisation_id_producer] /**  SN001: commented  **/
-			  ,[subsidiary_id]
-			  ,[CH_Number]
-			  ,[Nation_Of_Enrolment]
-			  ,[packaging_activity]
-			  ,[packaging_type]
-			  ,[packaging_class]
-			  ,[packaging_material]
-			  ,[packaging_sub_material]
-			  ,[from_nation]
-			  ,[to_nation]
-			  ,[quantity_kg]
-			  ,[quantity_unit]
-			  ,[Quantity_kg_extrapolated]
-			  ,[Quantity_units_extrapolated]
-			  ,[ToOrganisation_NationName]
-			  ,[Nation]
-			  ,[FromOrganisation_NationName]
-			  ,[FileName]
-			  ,[ServiceRoles_Role]
-			  ,[SubmittedBy]
-			  ,[filetype]
-			  ,[Users_Email]
-			  ,[Persons_Email]
-			  ,[metafile]
-			  ,[JOINFIELD]
-			  ,[relative_move]
-			  ,TransferNation
-			  ,[SubmtterEmail]
-			  ,[ServiceRoles_Name]
-			  ,[OriginalFileName] ,
-			  'Member'
-			  from v_POM_Operator_Submissions 
-		where	[organisation_id_producer] <>   [organisation_id]
-				AND compliance_scheme IS NOT NULL
-) A
+			SELECT
+				   [Org_Name]
+				   ,MemberName		= [Producer_Org_Name]  /**  SN001: Added  **/
+				  ,[PCS_Or_Direct_Producer]
+				  ,[Compliance_Scheme]
+				  ,[Org_Type]
+				  ,[Org_Sub_Type]
+				  ,[organisation_size]
+				  ,[Submission_Date]
+				  ,[submission_period]
+						,[organisation_id]  /**  SN001: Uncommented  **/
+				  --,[organisation_id_producer] /**  SN001: commented  **/
+				  ,[subsidiary_id]
+				  ,[CH_Number]
+				  ,[Nation_Of_Enrolment]
+				  ,[packaging_activity]
+				  ,[packaging_type]
+				  ,[packaging_class]
+				  ,[packaging_material]
+				  ,[packaging_sub_material]
+				  ,[from_nation]
+				  ,[to_nation]
+				  ,[quantity_kg]
+				  ,[quantity_unit]
+				  ,[Quantity_kg_extrapolated]
+				  ,[Quantity_units_extrapolated]
+				  ,[ToOrganisation_NationName]
+				  ,[Nation]
+				  ,[FromOrganisation_NationName]
+				  ,[FileName]
+				  ,[ServiceRoles_Role]
+				  ,[SubmittedBy]
+				  ,[filetype]
+				  ,[Users_Email]
+				  ,[Persons_Email]
+				  ,[metafile]
+				  ,[JOINFIELD]
+				  ,[relative_move]
+				  ,TransferNation
+				  ,[SubmtterEmail]
+				  ,[ServiceRoles_Name]
+				  ,[OriginalFileName] ,
+				  'Member'
+				  from v_POM_Operator_Submissions 
+			where	[organisation_id_producer] <>   [organisation_id]
+					AND compliance_scheme IS NOT NULL
+	) A
 left join dbo.v_submitted_pom_org_file_status d on d.Filename = A.FileName
+LEFT JOIN dbo.v_subsidiaryorganisations so 
+	on so.FirstOrganisation_ReferenceNumber = A.organisation_id
+		and ISNULL(trim(so.SubsidiaryId),'') = ISNULL(trim(A.subsidiary_id),'')
+			and so.RelationToDate is NULL
 )
 
 Select 
