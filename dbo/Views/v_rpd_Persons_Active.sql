035c1484a0a0480dadae96009a8b3b6e353d8ede
roshan-shaikh <161720098+roshan-shaikh@users.noreply.github.com>
2024-09-02 11:13:59 +0100

    Included changes are : (#83) (#84)
    1. US : 429684 Rejected enrolment of an approved person should show in enrolment report
    2. US : 422117Enrolment report -default to ALL & islatest in 'Latest' button

--- dbo/Views/v_rpd_Persons_Active.sql
+++ dbo/Views/v_rpd_Persons_Active.sql
@@ -1,4 +1,17 @@
-﻿CREATE VIEW [dbo].[v_rpd_Persons_Active]
-AS select * 
+﻿CREATE VIEW [dbo].[v_rpd_Persons_Active] AS select * 
 from rpd.Persons
-where IsDeleted = 0;
+where IsDeleted = 0
+
+UNION
+
+/*------------------------------------------------------------------------------------
+ * US 429684: Rejected enrolment of an approved person should show in enrolment report
+ *
+ * Don't treat IsDeleted = 1 as Soft deleted
+ *   when Role was 'Approved Person'
+ * ----------------------------------------------------------------------------------*/
+
+SELECT p.* 
+FROM [rpd].[Persons] p 
+JOIN [rpd].[PersonOrganisationConnections] poc ON poc.[PersonId] = p.[Id]
+WHERE poc.[PersonRoleId] = 1 AND p.[IsDeleted]=1;
