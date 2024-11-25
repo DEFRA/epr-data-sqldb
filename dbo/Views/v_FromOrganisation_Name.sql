1f032d9e75b2d8ac086b8a6dc77c479a4ddcee24
Roshan Shaikh <161720098+roshan-shaikh@users.noreply.github.com>
2024-11-25 15:41:22 +0000

    4 Views and 1 SP , checked in. (#105) (#106)
    Part of Release 6.1
    Timezone changes
    SP Optimization changes

--- dbo/Views/v_FromOrganisation_Name.sql
+++ dbo/Views/v_FromOrganisation_Name.sql
@@ -1,4 +1,4 @@
 ﻿CREATE VIEW [dbo].[v_FromOrganisation_Name] AS select distinct FromOrganisation_Name
 ,FromOrganisation_ReferenceNumber
-from v_rpd_data_SECURITY_FIX
+from t_rpd_data_SECURITY_FIX
 where FromOrganisation_Name is not null;
