2f7493bfc4e159b4daa0f68936d60205aef86f88
RAKESH MAMIDALA <rakesh.mamidala@eviden.com>
2024-12-16 17:55:11 +0000

    DT 1 SQL changes- Reg com

--- dbo/Views/v_Registration_Comparison_Landing_Page.sql
+++ dbo/Views/v_Registration_Comparison_Landing_Page.sql
@@ -4,9 +4,6 @@
 	,cset.[CompanyOriginalFileName]
 	,c.[SubmissionPeriod]
 	,c.[IsSubmitted]
-	--,CAST (CONCAT_WS('-', DATEPART(YYYY, c.[created] ), DATEPART(MM, c.[created] ), DATEPART(DD, c.[created] )) AS VARCHAR) + ' ' + CAST (CONCAT_WS(':', DATEPART(HH, c.[created] ), DATEPART(N, c.[created] ), DATEPART(SS, c.[created] )) AS VARCHAR)+'.'+CAST (DATEPART(MS, c.[created] ) AS VARCHAR)
-	--+ ' ' + cset.[CompanyOriginalFileName] + ' ' + 
-	--	      CAST(ISNULL(c.[SubmittedBy],'') AS VARCHAR) + ' ' +CAST(ISNULL(c.[SubmtterEmail],'') AS VARCHAR) + ' ' + CAST(ISNULL(c.[ServiceRoles_Name],'') AS VARCHAR) + ' ' + CAST(ISNULL(cset.[Regulator_Status],'') AS VARCHAR) AS FileCode
 	,CAST (CONCAT_WS('-', DATEPART(YYYY, c.[created] ), DATEPART(MM, c.[created] ), DATEPART(DD, c.[created] )) AS VARCHAR) + ' ' + CAST (CONCAT_WS(':', DATEPART(HH, c.[created] ), DATEPART(N, c.[created] ), DATEPART(SS, c.[created] )) AS VARCHAR)+'.'+CAST (DATEPART(MS, c.[created] ) AS VARCHAR)
 	+ ' ' + cset.[CompanyOriginalFileName] + ' ' + 
 		      CAST(ISNULL(c.[SubmittedBy],'') AS VARCHAR) + ' ' +CAST(ISNULL(c.[SubmtterEmail],'') AS VARCHAR) + ' ' + CAST(ISNULL(c.[ServiceRoles_Name],'') AS VARCHAR)  AS FileCode
@@ -24,7 +21,7 @@
 		ELSE 'Producer' END [CSORPD]
 	,'' AS Nation
 	,cset.[CompanyRegID]
-  FROM [dbo].[v_CompanyBrandPartnerFileUploadSet] cset
+  FROM [dbo].[t_CompanyBrandPartnerFileUploadSet] cset
   JOIN [dbo].[v_cosmos_file_metadata] c ON cset.[CompanyFileName] = c.[FileName]
   LEFT JOIN [rpd].[ComplianceSchemes] cs ON cs.[ExternalId] = cset.ComplianceSchemeId
   GROUP BY  cset.[CompanyOrgId]
