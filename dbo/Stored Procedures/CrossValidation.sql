75d3b0c9dd14199ad916d1cc0d49ed9361e01882
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-01-09 10:45:00 +0000

    Cross Validation Landing Page SQLS (#121) (#133)
    1. List of Views:
    [dbo].[v_CrossValidation_FileDetails_Main]
    [dbo].[v_CrossValidation_FileDetails_ORG]
    [dbo].[v_CrossValidation_FileDetails_POM]
    [dbo].[v_CrossValidation_FileDetails_Registration]
    
    2. List of Stored precedure:
    [dbo].[CrossValidation]
    [dbo].[CrossValidationGetLatestFile]
    
    Co-authored-by: Roshan Shaikh <161720098+roshan-shaikh@users.noreply.github.com>

---
+++ dbo/Stored Procedures/CrossValidation.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROC [dbo].[CrossValidation] @CS_or_DP [nvarchar](50),@RelevanYear [int] AS
+BEGIN
+
+ /*****************************************************************************************************************
+	History:
+
+	Created 2024-07-23: SN000:	Colate and compare Organisation and POM file details for cross validation report. 
+								Ticket 412664 (Crosss Validation)
+	
+	Updated 2024-07-24: SN001:	Logic added to compare only latest POM and ORG file if 'Direct Producer' selected
+	Updated 2024-07-24: SN002:	Changed Submission Year to ComplianceYear
+
+
+ *****************************************************************************************************************/
+
+
+	Select
+		/*** File Data ***/
+		 fd.FileName_Guid
+		,fd.FileType
+		,fd.OriginalFileName
+		,fd.DisplayFilenameTS
+		,fd.ComplianceSchemeName
+		,fd.CS_or_DP
+		,fd.SubmissionPeriod
+		,fd.RelevantYear
+		,fd.SubmissionType
+		,fd.TargetDirectoryName
+		,fd.LatestFileBySubmissionPeriod
+		,fd.Regulator_Status
+	From
+		dbo.v_CrossValidation_FileDetails_Registration	fd
+	Where
+	    fd.CS_or_DP = @CS_or_DP and fd.RelevantYear = @RelevanYear
+END
