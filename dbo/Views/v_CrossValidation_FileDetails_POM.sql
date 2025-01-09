2ddb5fc5801422ff46ef05238ee54bb3fd0f6e11
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-01-09 11:40:43 +0000

    Cross Validation Landing Page SQLS (#121) (#136)
    1. List of Views:
    [dbo].[v_CrossValidation_FileDetails_Main]
    [dbo].[v_CrossValidation_FileDetails_ORG]
    [dbo].[v_CrossValidation_FileDetails_POM]
    [dbo].[v_CrossValidation_FileDetails_Registration]
    
    2. List of Stored precedure:
    [dbo].[CrossValidation]
    [dbo].[CrossValidationGetLatestFile]
    
    Co-authored-by: Roshan Shaikh <161720098+roshan-shaikh@users.noreply.github.com>

---
+++ dbo/Views/v_CrossValidation_FileDetails_POM.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [dbo].[v_CrossValidation_FileDetails_POM]
+AS Select
+/*****************************************************************************************************************
+	History:
+
+	Created 2024-07-18: SN000: File Details for ORG 
+								Ticket 404904,412664 (Crosss Validation)
+
+	Updated 2024-07-25: SN001: ComplianceYear Added
+
+*****************************************************************************************************************/
+	 FileId
+	,FileType
+	,DisplayFilenameTS
+	,ComplianceSchemeName
+	,CS_or_DP
+	,SubmissionPeriod
+	,ComplianceYear
+	,LatestFileBySubmissionPeriod
+	,Regulator_Status
+From
+	v_CrossValidation_FileDetails_Main
+Where
+	FileType='POM';
