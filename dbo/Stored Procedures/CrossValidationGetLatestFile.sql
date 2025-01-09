38d719c426f098dc104c8c71fc59131e46b65c86
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-01-09 10:45:00 +0000

    Cross Validation Landing Page SQLS (#121) (#133)
    1. List of Views:
    [dbo].[v_CrossValidation_FileDetails_Main]
    [dbo].[v_CrossValidation_FileDetails_ORG]
    [dbo].[v_CrossValidation_FileDetails_POM]
    [dbo].[v_CrossValidation_FileDetails_Registration]
    
    2. List of Stored precedure:
    [dbo].[CrossValidation]
    [dbo].[CrossValidationGetLatestFile]
    
    Co-authored-by: Roshan Shaikh <161720098+roshan-shaikh@users.noreply.github.com>

---
+++ dbo/Stored Procedures/CrossValidationGetLatestFile.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROC [dbo].[CrossValidationGetLatestFile] @FileTYpe [nvarchar](50),@FileNname [nvarchar](4000) OUT AS
+BEGIN
+/*****************************************************************************************************************
+	History:
+
+	Created 2024-07-23: SN000:	This stored procudure get latest file (based on file type) to Colate and compare Organisation 
+								and POM file details for cross validation report. 
+								Ticket 412664 (Crosss Validation)
+	
+	Updated 2024-07-24: SN001: Logic added to compare only latest POM and ORG file if 'Direct Producer' selected
+
+	Updated 2024-MM-DD: [Initials]002: Change Description
+
+*****************************************************************************************************************/
+
+	Select
+		@FileNname = DisplayFilenameTS
+	From
+		dbo.v_CrossValidation_FileDetails_Main	src
+	Where
+		src.LatestSubmissionFile = 1 And src.FileType = @FileTYpe;
+END
