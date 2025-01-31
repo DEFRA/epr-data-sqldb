8fcbae6671175ce1d9fb3ba0bb3a86d55b3ca0ab
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-01-31 13:16:01 +0000

    500555 - fix (#160)
    Rugulator journey bug fix for R7

--- dbo/Views/v_PRN_Details.sql
+++ dbo/Views/v_PRN_Details.sql
@@ -4,7 +4,9 @@
  
 	Created: 2024-11-20:	SN001:	Ticket - 464576:	Creation of View for PRN details for POwer BI reporting
 	Updated: 2024-12-13		SN002:						Typo on nation description
-	Updated: 2025-01-13		SN003:						Include Ability to map CSIds 
+	Updated: 2025-01-13		SN003:						Include Ability to map CSIds
+	Updated: 2025-01-29		SN004:	Ticket  - 501685	Remove Duplicate Status coming from PrnStatusHistory
+	Updated: 2025-01-30		SN005:	Ticket  - 501253	ComplianceSchemes not in Metafile data joined by Companies House no.
 
 ******************************************************************************************************************************/
 	Select 
@@ -12,6 +14,7 @@
 		,PrnIdFk				= sth.PrnIdFk
 		,PrnStatusName			= st.StatusName 
 		,PrnStatusDate			= sth.CreatedOn
+		,IsLatest				= Row_Number() Over (Partition By sth.PrnIdFk Order By sth.Id Desc)  /*** SN0004: Added ***/
 	From
 		rpd.PrnStatusHistory	sth
 	Left Join 
@@ -26,10 +29,10 @@ st_pvt As (
 		,PrnRejectedDate			= REJECTED	
 		,PrnCancelledDate			= CANCELLED	
 		,PrnAwaitingAcceptanceDate	= AWAITINGACCEPTANCE
-	From  st
+	From  st 
 	Pivot (		
 			Max(PrnStatusDate) for PrnStatusName In ([ACCEPTED],[REJECTED],[CANCELLED],[AWAITINGACCEPTANCE])
-	) stp
+	) stp Where IsLatest=1			/*** SN0004: Added ***/
 ), 
 org As (  /*** SN:003 ***/
 	Select
@@ -42,6 +45,20 @@ org As (  /*** SN:003 ***/
 		,RowNumber	=1
 	From
 		dbo.v_rpd_Organisations_Active	o
+),
+cs_ch As (  /*** SN005: Added ***/
+	Select
+	 cs.ExternalID
+	,o.ReferenceNumber
+	,o.NationId
+	,o.IsComplianceScheme
+	,o.Town
+	,o.Postcode 
+	,RowNumber	=1
+From
+	dbo.v_rpd_Organisations_Active	o
+Join
+	rpd.ComplianceSchemes			cs On o.CompaniesHouseNumber = cs.CompaniesHouseNumber
 ),
 csa As (
 	Select  
@@ -82,6 +99,17 @@ org_csa As (
 	From 
 		csa 
 	Where csa.RowNumber = 1
+	Union
+	Select /*** SN005: Added ***/
+		 cs_ch.ExternalID			
+		,cs_ch.ReferenceNumber	
+		,cs_ch.NationId
+		,cs_ch.IsComplianceScheme 
+		,cs_ch.Town
+		,cs_ch.PostCode
+	From 
+		cs_ch 
+	
 ), /*** SN:003 ***/
 prn As (
 	Select 
@@ -125,13 +153,13 @@ prn As (
 		rpd.Prn					p
 	Left Join
 		st_pvt					st
-			On p.Id = st.PrnIdFk
+			On p.Id = st.PrnIdFk 
 	Left Join
 		rpd.PrnStatus			s
 			On p.PrnStatusId = s.id
 )
 
-Select
+Select p.id,
 	 p.PrnNumber
 	,p.ExternalOrgId
 	,OrganisationId					= o.[ReferenceNumber]
