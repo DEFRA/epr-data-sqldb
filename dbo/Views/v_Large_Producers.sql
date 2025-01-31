19907f93a807c73451260503178261e92de1f4bd
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-01-31 09:24:46 +0000

    PLLP fix for tickets 501627, 501622 (#159)

--- dbo/Views/v_Large_Producers.sql
+++ dbo/Views/v_Large_Producers.sql
@@ -1,10 +1,12 @@
-﻿CREATE VIEW [dbo].[v_Large_Producers] AS with all_org_with_status as
+﻿CREATE VIEW [dbo].[v_Large_Producers] AS with 
+all_org_with_status as
 (
 	select distinct cd.organisation_id
 					, cd.filename
 					, CAST(CONVERT(datetimeoffset, meta.created) as datetime) AS submitted_time
-					,'20'+reverse(substring(reverse(trim(meta.SubmissionPeriod)),1,2)) as SubmissionYear
-					,file_status.Regulator_Status
+					, '20'+reverse(substring(reverse(trim(meta.SubmissionPeriod)),1,2)) as SubmissionYear
+					, upper(trim(isnull(file_status.Regulator_Status,''))) as Regulator_Status
+					, 'cd' as filetype
 	from [rpd].[CompanyDetails] cd
 	left join [dbo].[v_submitted_pom_org_file_status] file_status on file_status.FileName = cd.filename
 	left join [rpd].[cosmos_file_metadata] meta on meta.filename = cd.filename
@@ -14,106 +16,30 @@ all_pom_with_status as
 	select distinct pom.organisation_id
 					, pom.filename
 					, CAST(CONVERT(datetimeoffset, meta.created) as datetime) AS submitted_time
-					,'20'+reverse(substring(reverse(trim(meta.SubmissionPeriod)),1,2)) as SubmissionYear
-					,file_status.Regulator_Status
+					, '20'+reverse(substring(reverse(trim(meta.SubmissionPeriod)),1,2)) as SubmissionYear
+					, upper(trim(isnull(file_status.Regulator_Status,''))) as Regulator_Status
+					, 'pm' as filetype
 	from [rpd].[Pom] pom
 	left join [dbo].[v_submitted_pom_org_file_status] file_status on file_status.FileName = pom.filename
 	left join [rpd].[cosmos_file_metadata] meta on meta.filename = pom. filename
 ),
-all_accepted_org as
+all_pending_or_accepted_org_pom_files as
 (
-	select * from all_org_with_status where upper(Regulator_Status) = 'ACCEPTED'
+	select * From all_org_with_status where Regulator_Status = 'ACCEPTED' or Regulator_Status = ''
+	union 
+	select * From all_pom_with_status where Regulator_Status = 'ACCEPTED' or Regulator_Status = ''
 ),
-all_accepted_org_with_rank as
+all_pending_or_accepted_org_pom_files_with_rank as
 (
 	select *
-		, row_number() over(partition by organisation_id, SubmissionYear order by submitted_time desc) as cd_rn
-	from all_accepted_org
+		, row_number() over(partition by organisation_id, SubmissionYear order by filetype asc, submitted_time desc) as rn
+	from all_pending_or_accepted_org_pom_files	
 ),
-latest_accepted_org as
-(
-	select * from all_accepted_org_with_rank where cd_rn = 1
-),--till here it is fine (1)
-all_accepted_pom as
-(
-	select * from all_pom_with_status where upper(Regulator_Status) = 'ACCEPTED'
-),
-all_accepted_pom_without_org as
-(
-	select all_accepted_pom.* 
-	from all_accepted_pom
-	left join all_org_with_status 
-		on all_org_with_status.organisation_id = all_accepted_pom.organisation_id 
-		and all_accepted_pom.SubmissionYear = all_org_with_status.SubmissionYear
-		and ISNULL(upper(all_org_with_status.Regulator_Status),'') <> 'REJECTED'
-	where all_org_with_status.organisation_id is null 
-
-),
-all_accepted_pom_without_org_with_rank as 
+all_pending_or_accepted_org_pom_files_with_rank_as_1 as
 (
 	select *
-		, row_number() over(partition by organisation_id, SubmissionYear order by submitted_time desc) as cd_rn
-	from all_accepted_pom_without_org
-),
-latest_accepted_pom_without_org as
-(
-	select * from all_accepted_pom_without_org_with_rank where cd_rn = 1
-),--this is fine (2)
-all_pending_andaccepted_pom as
-(
-	select * from all_pom_with_status where upper(ISNULL(Regulator_Status,'')) in ('', 'ACCEPTED')
-),
-all_pending_and_accepted_org as
-(
-	select * from all_org_with_status where upper(ISNULL(Regulator_Status,'')) in ('', 'ACCEPTED')
-),
-all_pending_and_accepted_org_With_pom as
-(
-	select distinct all_pending_and_accepted_org.* 
-	from all_pending_and_accepted_org
-	/*
-	inner join all_pending_andaccepted_pom 
-		on all_pending_and_accepted_org.organisation_id = all_pending_andaccepted_pom.organisation_id
-		and all_pending_and_accepted_org.SubmissionYear = all_pending_andaccepted_pom.SubmissionYear
-	*/
-),
-all_pending_and_accepted_org_With_pom_removed_case1_case2 as
-(
-	select distinct all_pending_and_accepted_org_With_pom.organisation_id,	
-		all_pending_and_accepted_org_With_pom.filename,	
-		all_pending_and_accepted_org_With_pom.submitted_time,	
-		all_pending_and_accepted_org_With_pom.SubmissionYear,	
-		upper(ISNULL(all_pending_and_accepted_org_With_pom.Regulator_Status,'PENDING')) as Regulator_Status
-	from all_pending_and_accepted_org_With_pom
-	left join latest_accepted_org
-		on latest_accepted_org.organisation_id = all_pending_and_accepted_org_With_pom.organisation_id
-		and latest_accepted_org.SubmissionYear = all_pending_and_accepted_org_With_pom.SubmissionYear
-	left join latest_accepted_pom_without_org
-		on latest_accepted_pom_without_org.organisation_id = all_pending_and_accepted_org_With_pom.organisation_id
-		and latest_accepted_pom_without_org.SubmissionYear = all_pending_and_accepted_org_With_pom.SubmissionYear
-	where latest_accepted_org.organisation_id is null
-	and latest_accepted_pom_without_org.organisation_id is null
-),
-all_pending_and_accepted_org_With_pom_removed_case1_case2_with_rank as
-(
-	select *
-		, row_number() over(partition by organisation_id, SubmissionYear order by Regulator_Status, submitted_time desc) as cd_rn
-	from all_pending_and_accepted_org_With_pom_removed_case1_case2
-),
-all_pending_and_accepted_org_With_pom_removed_case1_case2_with_rank_as_1 as
-(
-	select * from all_pending_and_accepted_org_With_pom_removed_case1_case2_with_rank
-	where cd_rn = 1
-),--this is fine (3)
-final_org as
-(
-	select *,'1' as criteria from latest_accepted_org
-	union
-	select *,'3' as criteria from all_pending_and_accepted_org_With_pom_removed_case1_case2_with_rank_as_1		
-),
-final_pom as
-(
-	select *,'2' as criteria from latest_accepted_pom_without_org
+	from all_pending_or_accepted_org_pom_files_with_rank
+	where rn = 1
 ),
 pom_result as
 (
@@ -176,7 +102,7 @@ pom_result as
 			END) As 'Compliance_scheme_regulator'
 		,p.SubmissionYear as 'Reporting_year'
 		,meta.created SubmittedDateTime
-	from final_pom p
+	from all_pending_or_accepted_org_pom_files_with_rank_as_1 p
 		inner join [rpd].[Pom] pm 
 			on pm.FileName = p.FileName
 				and p.organisation_id = pm.organisation_id
@@ -198,7 +124,8 @@ pom_result as
 						FROM t_rpd_data_SECURITY_FIX
 						GROUP BY FromOrganisation_ReferenceNumber, EnrolmentStatuses_EnrolmentStatus) e_status
 			ON e_status.FromOrganisation_ReferenceNumber = p.organisation_id
-		where  pm.organisation_size = 'L'
+		where p.filetype = 'pm'
+				AND (pm.organisation_size = 'L' or pm.organisation_size IS NULL or trim(pm.organisation_size) = '')
 				AND (cs.IsDeleted = 0 OR cs.IsDeleted IS NULL)  ---> If only company-details file is submitted cs.IsDeleted would be NULL
 				AND (prr.isdeleted = 0 OR prr.isdeleted IS NULL)
 				AND e_status.EnrolmentStatuses_EnrolmentStatus <> 'Rejected'
@@ -265,7 +192,7 @@ org_result as
 					END) As 'Compliance_scheme_regulator'
 				,cd.SubmissionYear as 'Reporting_year'
 				, meta.created SubmittedDateTime
-			FROM final_org cd
+			FROM all_pending_or_accepted_org_pom_files_with_rank_as_1 cd
 			inner join [rpd].[CompanyDetails] cds 
 				on cds.Filename = cd.Filename
 					and cds.organisation_id = cd.organisation_id
@@ -287,7 +214,9 @@ org_result as
 					FROM t_rpd_data_SECURITY_FIX
 					GROUP BY FromOrganisation_ReferenceNumber, EnrolmentStatuses_EnrolmentStatus) e_status
 				ON e_status.FromOrganisation_ReferenceNumber = cd.organisation_id
-				WHERE (cs.IsDeleted = 0 OR cs.IsDeleted IS NULL)
+				WHERE cd.filetype = 'cd'
+					AND (cds.organisation_size = 'L' or cds.organisation_size IS NULL or trim(cds.organisation_size) = '')
+					AND (cs.IsDeleted = 0 OR cs.IsDeleted IS NULL)
 					AND (pr.isdeleted = 0 OR pr.isdeleted IS NULL)
 					AND e_status.EnrolmentStatuses_EnrolmentStatus <> 'Rejected'
 					AND (pr.IsComplianceScheme = 0 OR pr.IsComplianceScheme IS NULL)
@@ -298,15 +227,10 @@ org_result as
 				RPD_Organisation_ID,		submission_period,		Compliance_scheme,		Companies_House_number,		Subsidiary_ID,		Organisation_name,
 				Trading_name,		Address_line_1,		Address_line_2,		Address_line_3,		Address_line_4,		Town,		County,		Country,
 				Postcode,		ProducerNation,		ProducerNationId,		ComplianceSchemeNation,		ComplianceSchemeNationId,		ProducerId,
-				Environmental_regulator,		Compliance_scheme_regulator,		Reporting_year
-		
+				Environmental_regulator,		Compliance_scheme_regulator,		Reporting_year	
 			from
 			(
-				select *, Row_number() over(Partition by RPD_Organisation_ID, Subsidiary_ID, Reporting_year order by SubmittedDateTime desc) as rn
-				from
-				(	
 					select * from pom_result
 					union
 					select * from org_result
-				) A
 			) B;
