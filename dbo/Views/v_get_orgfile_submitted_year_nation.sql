18215f9637e71121e839e41cf1956eb06b070bb7
tufanshn <tufan.sahin.external@eviden.com>
2025-03-04 16:46:44 +0000

    update Relevant year

--- dbo/Views/v_get_orgfile_submitted_year_nation.sql
+++ dbo/Views/v_get_orgfile_submitted_year_nation.sql
@@ -3,14 +3,20 @@
 FROM
 	(
 	
-	SELECT  '20' + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2))+1 AS Relevant_Year,
+	SELECT 
+	CASE
+		WHEN '20' + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2)) = 2023 then 2023+1
+		WHEN '20' + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2)) = 2024 then 2024+1
+		ELSE '20' + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2)) END AS Relevant_Year,
 					  Organisation_Nation_Name,
 					  CS_Nation_name,
 					  'Link Column' as Link_Column
 	FROM   [dbo].[v_latest_pending_or_accepted_orgfile_by_year]
 	UNION ALL
-	SELECT  '20'
-	                + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2))+1 AS Relevant_Year,
+	SELECT  CASE
+				WHEN '20' + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2)) = 2023 then 2023+1
+				WHEN '20' + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2)) = 2024 then 2024+1
+				ELSE '20' + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2)) END AS Relevant_Year,
 					Organisation_Nation_Name,
 					CS_Nation_name,
 					'Link Column' as Link_Column
