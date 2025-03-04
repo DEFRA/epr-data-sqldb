1214320cdfdea249c7ad7af5312cd475d4bb9e8b
tufanshn <tufan.sahin.external@eviden.com>
2025-03-04 16:02:38 +0000

    Joiners and leavers report - nation column is added

---
+++ dbo/Views/v_get_orgfile_submitted_year_nation.sql
@@ -0,0 +1,18 @@
+﻿CREATE VIEW [dbo].[v_get_orgfile_submitted_year_nation] AS SELECT DISTINCT Relevant_Year, Organisation_Nation_Name, CS_Nation_name, Link_Column
+
+FROM
+	(
+	
+	SELECT  '20' + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2))+1 AS Relevant_Year,
+					  Organisation_Nation_Name,
+					  CS_Nation_name,
+					  'Link Column' as Link_Column
+	FROM   [dbo].[v_latest_pending_or_accepted_orgfile_by_year]
+	UNION ALL
+	SELECT  '20'
+	                + Reverse(Substring(Reverse(Trim(SubmissionPeriod)), 1, 2))+1 AS Relevant_Year,
+					Organisation_Nation_Name,
+					CS_Nation_name,
+					'Link Column' as Link_Column
+	FROM   [dbo].[v_latest_accepted_orgfile_by_year]
+	) A;
