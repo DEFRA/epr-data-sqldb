258ddeb3866aec8b65f762026875bd06d80e3cff
Prasanna Thakku Mani <prasanna.thakkumani@eviden.com>
2025-03-11 12:46:21 +0000

    515351 - t table changed to add columns

--- dbo/Tables/t_latest_pending_or_accepted_orgfile_by_year.sql
+++ dbo/Tables/t_latest_pending_or_accepted_orgfile_by_year.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[t_latest_pending_or_accepted_orgfile_by_year] (
     [file_submitted_organisation_reference]          NVARCHAR (4000) NULL,
     [file_submitted_organisation_IsComplianceScheme] BIT             NULL,
+    [CS_Nation_name]                                 NVARCHAR (4000) NULL,
     [meta_OrganisationId]                            NVARCHAR (4000) NULL,
     [SubmissionPeriod]                               NVARCHAR (4000) NULL,
     [ReportingYear]                                  NVARCHAR (4)    NULL,
@@ -23,10 +24,26 @@
     [registered_addr_postcode]                       NVARCHAR (4000) NULL,
     [registered_addr_country]                        NVARCHAR (4000) NULL,
     [registered_addr_phone_number]                   NVARCHAR (4000) NULL,
+    [approved_person_first_name]                     NVARCHAR (4000) NULL,
+    [approved_person_last_name]                      NVARCHAR (4000) NULL,
     [approved_person_email]                          NVARCHAR (4000) NULL,
+    [approved_person_phone_number]                   NVARCHAR (4000) NULL,
+    [delegated_person_first_name]                    NVARCHAR (4000) NULL,
+    [delegated_person_last_name]                     NVARCHAR (4000) NULL,
     [delegated_person_email]                         NVARCHAR (4000) NULL,
+    [delegated_person_phone_number]                  NVARCHAR (4000) NULL,
+    [primary_contact_person_first_name]              NVARCHAR (4000) NULL,
+    [primary_contact_person_last_name]               NVARCHAR (4000) NULL,
+    [primary_contact_person_email]                   NVARCHAR (4000) NULL,
+    [primary_contact_person_phone_number]            NVARCHAR (4000) NULL,
     [Subsidiary_RelationFromDate]                    DATETIME2 (7)   NULL,
-    [Subsidiary_RelationToDate]                      DATETIME2 (7)   NULL
+    [Subsidiary_RelationToDate]                      DATETIME2 (7)   NULL,
+    [Organisation_Nation_Name]                       NVARCHAR (4000) NULL,
+    [Organisation_Nation_Id]                         INT             NULL,
+    [joiner_date]                                    NVARCHAR (4000) NULL,
+    [leaver_code]                                    NVARCHAR (4000) NULL,
+    [leaver_date]                                    NVARCHAR (4000) NULL,
+    [Organisation_change_reason]                     VARCHAR (1)     NOT NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
 
