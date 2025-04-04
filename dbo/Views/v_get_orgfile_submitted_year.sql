72304c33255db8f7bc401e8444e7ba4c89c4c673
Prasanna Thakku Mani <prasanna.thakkumani@eviden.com>
2025-04-04 17:12:32 +0100

    Removing JL changes from R9

--- dbo/Views/v_get_orgfile_submitted_year.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE VIEW [dbo].[v_get_orgfile_submitted_year] AS SELECT DISTINCT '20'
-                  + Reverse(Substring(Reverse(Trim(submissionperiod)), 1, 2)) AS ReportingYear
-  FROM   rpd.cosmos_file_metadata
-  WHERE  Upper(filetype) = 'COMPANYDETAILS';
