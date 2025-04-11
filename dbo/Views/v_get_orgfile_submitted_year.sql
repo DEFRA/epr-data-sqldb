e0f2dcc3829ad43916423f325e328ecf64d4a79e
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-04-11 14:56:11 +0100

    Revert "Removing JL changes from R9 (#239)" (#255)
    This reverts commit 4132aea9c4c59e3b985aef2502076be0bffdcdd8.

---
+++ dbo/Views/v_get_orgfile_submitted_year.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [dbo].[v_get_orgfile_submitted_year] AS SELECT DISTINCT '20'
+                  + Reverse(Substring(Reverse(Trim(submissionperiod)), 1, 2)) AS ReportingYear
+  FROM   rpd.cosmos_file_metadata
+  WHERE  Upper(filetype) = 'COMPANYDETAILS';
