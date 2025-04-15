02d3ff7a0eb0958fbb844dd87a83b3611f2f8a99
Rakesh Mamidala <156447227+rakeshmamidala221@users.noreply.github.com>
2025-04-15 18:30:58 +0100

    FE stored Procs (#263) (#264)

---
+++ dbo/Stored Procedures/sp_CheckForCosmosData.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROC [dbo].[sp_CheckForCosmosData] @SubmissionId [nvarchar](50),@FileId [nvarchar](50) AS
+begin
+	DECLARE @IsSynced BIT = 0; -- Default value
+
+	IF @SubmissionId IS NOT NULL
+	BEGIN
+		SELECT @IsSynced = CAST(CASE WHEN EXISTS (
+			SELECT 1 FROM rpd.cosmos_file_metadata 
+			WHERE SubmissionId = @SubmissionId
+		) THEN 1 ELSE 0 END AS BIT);
+	END
+	ELSE IF @FileId IS NOT NULL
+	BEGIN
+		SELECT @IsSynced = CAST(CASE WHEN EXISTS (
+			SELECT 1 FROM rpd.cosmos_file_metadata 
+			WHERE FileId = @FileId
+		) THEN 1 ELSE 0 END AS BIT);
+	END
+
+	-- Return the result
+	SELECT @IsSynced AS IsSynced;
+end
