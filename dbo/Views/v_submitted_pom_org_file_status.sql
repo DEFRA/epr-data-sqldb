e6936277060ade1e2a00b8e7d6fe34bb87104d7e
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-04-25 09:54:52 +0100

    543160 status view fix [dbo].[v_submitted_pom_org_file_status] (#274)

--- dbo/Views/v_submitted_pom_org_file_status.sql
+++ dbo/Views/v_submitted_pom_org_file_status.sql
@@ -17,13 +17,19 @@
 	Updated: 2025-03-19:	PM004:	Ticket - 512853		Bring the Registration file status as 'Uploaded' if not fully submitted in front end. This is the status before pending.
 	Updated: 2025-03-24:	SN005:	Ticket - 520206		Add Application/App Reference Number to be passed to output for display on Power BI Report
 	Updated: 2025-04-03:	RM006:	Ticket - 527578		Add registrationreferencenumber to be passed on to v_public_register_all_producers
+	Updated: 2025-04-24:	PM006:	Ticket - 543160		Rewritten to handle status as per the ticekt 543160
 ******************************************************************************************************************************/
+ 
+--Sub query to find the resubmission records
 resubmission_ids as
 (
 select distinct ISNULL(IsResubmission,0) as IsResubmission_identifier, Fileid 
 from rpd.SubmissionEvents
 where Fileid is not null and IsResubmission is not null
 ),
+
+
+--Find file id for the records that are missing fileid in decision records
 null_fileid_decision_records as
 (
 select CONVERT(DATETIME,substring(Created,1,23)) as Created_ts , SubmissionEventId, SubmissionId
@@ -53,225 +59,120 @@ SubmissionEvents_updated as
 	select se.*, fs.fileid_new
 	from rpd.SubmissionEvents se
 	left join final_result_set fs on fs.SubmissionId = se.SubmissionId and fs.SubmissionEventId = se.SubmissionEventId
-)
-,
-RegSubDate As
-/*** SN002: Added 501408 - Retrieves rows with regulator SubmissionDate values  Lastest type='RegistrationApplicationSubmitted' ***/
+),
+
+
+--Find fileid that are submitted
+submitted_Fileids as
 (
-	Select
-		 se.SubmissionId
-		,se.ApplicationReferenceNumber
-		,se.created 
-		,se.[Type]
-		,RowNo		= Row_Number() Over(Partition By se.SubmissionId Order By se.created Desc)
-	From
-		rpd.SubmissionEvents		se
-	Where
-		se.[type] in ('RegistrationApplicationSubmitted') And se.ApplicationReferenceNumber is not null
+	select distinct Fileid as submitted_Fileid, SubmissionEventId as SubmissionEventId_of_submitted_record, SubmissionId as SubmissionId_of_submitted_record, CONVERT(DATETIME,substring(Created,1,23)) as Submitted_ts
+	From rpd.SubmissionEvents where Type = 'Submitted' and Fileid is not null
+),
+
+--File file id matching to the RegistrationApplicationSubmitted
+get_all_RegistrationApplicationSubmitted as
+(
+	select distinct sub.submitted_Fileid as app_submitted_Fileid, sub.SubmissionEventId_of_submitted_record
+		, app_sub.SubmissionEventId as SubmissionEventId_of_application_submitted_record
+		, CONVERT(DATETIME,substring(app_sub.Created,1,23)) as Submitted_ts
+		, app_sub.ApplicationReferenceNumber
+		, row_number() over(partition by app_sub.SubmissionId, app_sub.SubmissionEventId order by sub.Submitted_ts desc) as RN
+	From rpd.SubmissionEvents app_sub
+		 inner join submitted_Fileids sub 
+			on sub.SubmissionId_of_submitted_record = app_sub.SubmissionId
+				and CONVERT(DATETIME,substring(app_sub.Created,1,23)) >= sub.Submitted_ts 
+	where app_sub.Type = 'RegistrationApplicationSubmitted' 
+),
+top_matching_og_get_all_RegistrationApplicationSubmitted as
+(
+	select * From get_all_RegistrationApplicationSubmitted where RN = 1
 )
 
-, 
-se As (
+,res as (
+		select distinct
+				 cfm.[SubmissionId]
+				,cfm.[RegistrationSetId]
+				,cfm.[OrganisationId]	
+				,cfm.[FileName]
+				,cfm.[FileType]
+				,cfm.[OriginalFileName]
+				,cfm.[TargetDirectoryName]
+				, se.created as Decision_Date
+				, case when Right(dbo.udf_DQ_SubmissionPeriod(s.SubmissionPeriod),4) >= 2025 and ISNULL(rid.IsResubmission_identifier,0) = 0 and se.Decision = 'Accepted' Then 'Granted'
+					   when Right(dbo.udf_DQ_SubmissionPeriod(s.SubmissionPeriod),4) >= 2025 and ISNULL(rid.IsResubmission_identifier,0) = 0 and se.Decision = 'Rejected' Then 'Refused'
+					   when app_submitted.SubmissionEventId_of_application_submitted_record is not null and se.Decision is null then 'Pending'
+					   when app_submitted.SubmissionEventId_of_application_submitted_record is null and se.Decision is null and Right(dbo.udf_DQ_SubmissionPeriod(s.SubmissionPeriod),4) >= 2025 then 'Uploaded'
+					   when app_submitted.SubmissionEventId_of_application_submitted_record is null and se.Decision is null and Right(dbo.udf_DQ_SubmissionPeriod(s.SubmissionPeriod),4) < 2025  then 'Pending'
+					   else se.Decision
+					   end
+					as Regulator_Status
+				,'' AS [RegulatorDecision] --not represented in Cosmos DB
+				, ISNULL(p.[FirstName],'') +' '+ ISNULL(p.[LastName],'') as Regulator_User_Name
+				, se.Comments as Regulator_Rejection_Comments
+				, '' as RejectionComments
 
-	Select
-		 cfm.FileId
-		,se.SubmissionId
-		,se.AppReferenceNumber
-		,se.ApplicationReferenceNumber				/*** SN005: Added ***/
-		,Decision_Date					= se.[created]   
-		,Regulator_Status				= Case 
-											When ISNULL(rid.IsResubmission_identifier,0) = 0 and se.[type] = 'RegulatorRegistrationDecision' and cfm.SubmissionPeriod not in ('January to June 2023','January to June 2024','July to December 2023','July to December 2024') and se.[Decision] = 'Accepted' Then 'Granted'
-										    When ISNULL(rid.IsResubmission_identifier,0) = 0 and se.[type] = 'RegulatorRegistrationDecision' and cfm.SubmissionPeriod not in ('January to June 2023','January to June 2024','July to December 2023','July to December 2024') and se.[Decision] = 'Rejected' Then 'Refused' 
-											Else se.[Decision]
-										 End 
-		,Regulator_Rejection_Comments	= se.[Comments] 
-		,RejectionComments				= ''  --not represented in Cosmos DB
-		,se.[Type]
-		,se.[UserId]
-		,cfm.Created as Created
-		--,Created	= NULL							/*** SN002: Added 501408 - SubmissionDate ***/
-		,RegistrationType					= 1		/*** SN002: Added 501408 - To allow logic in PowerBI Regulator_Status to be set to Pending ***/
-		,se.registrationreferencenumber
-		--,rid.IsResubmission_identifier
-	From
-		rpd.cosmos_file_metadata	cfm
-	left join resubmission_ids rid on rid.fileid = cfm.fileid
-	inner Join
-		SubmissionEvents_updated		se
-			on cfm.FileId = ISNULL(se.FileId,se.fileid_new) and se.[type] in ('RegulatorPoMDecision', 'RegulatorRegistrationDecision')
-	--where cfm.SubmissionId = '10a15b92-57cf-48ec-885d-c170202d8933'
+				, case when se.[Type] is null 
+							and SubmissionEventId_of_application_submitted_record is not null 
+							then 'RegistrationApplicationSubmitted'
+						when se.[Type] is null
+							and sfs.SubmissionEventId_of_submitted_record is not null
+							then 'Submitted'
+						else se.[Type]
+						end as [Type]
+				, se.UserId
+				, se.Created
+				, case when Right(dbo.udf_DQ_SubmissionPeriod(s.SubmissionPeriod),4) >= 2025 and s.SubmissionType = 'Registration' then 2
+					   when Right(dbo.udf_DQ_SubmissionPeriod(s.SubmissionPeriod),4) < 2025 then 1
+						else NULL
+						end
+					as RegistrationType 
+				,s.SubmissionPeriod
+				, coalesce(app_submitted.ApplicationReferenceNumber,se.AppReferenceNumber) as ApplicationReferenceNo
+				, se.registrationreferencenumber
+				--Supporting columns
+				, se.Decision as Original_Regulator_Status
 		
-	
-/*
-	Union
-	Select
-		 cfm.FileId
-		,se.SubmissionId
-		,se.AppReferenceNumber
-		,se.ApplicationReferenceNumber				/*** SN005: Added ***/
-		,Decision_Date					= se.[created] 
-		,Regulator_Status				= Case se.[Decision] /*** SN002: Added 501408 ***/
-											When 'Accepted' Then 'Granted'
-										    When 'Rejected' Then 'Refused' /***  SN002, YM001: Implementation TBC 501408 ,Rejected to Refused Status change***/
-											Else se.[Decision]
-										 End 
-		,Regulator_Rejection_Comments	=  se.[Comments]					
-		,RejectionComments				= ''  --not represented in Cosmos DB
-		,se.[Type]
-		,se.[UserId]
-		,rsd.Created								/*** SN002: Added 501408 -- SubmissionDate ***/
-		,RegistrationType				= 2			/*** SN002: Added 501408 - To allow logic in PowerBI Regulator_Status to be set to NULL ***/
-		,se.registrationreferencenumber
-	From
-		rpd.cosmos_file_metadata	cfm
-	Left Join
-		SubmissionEvents_updated		se
-			on cfm.FileId = ISNULL(se.FileId,se.fileid_new) 
-	Left Join
-		RegSubDate					rsd
-			on se.SubmissionId = rsd.SubmissionId And rsd.RowNo=1
-Where
-se.[type] in ('RegulatorRegistrationDecision') And 
-se.AppReferenceNumber is not null
+				,s.SubmissionType
+				, ISNULL(rid.IsResubmission_identifier,0) as IsResubmission_identifier
+				, cfm.FileId as cfm_FileId
+				, se.FileId
+				, se.fileid_new
+				, sfs.submitted_Fileid
+				, sfs.SubmissionEventId_of_submitted_record
+				, app_submitted.app_submitted_Fileid 
+				, app_submitted.SubmissionEventId_of_application_submitted_record
 
-*/
-	union
-	Select
-		 cfm.FileId
-		,se.SubmissionId
-		,se.AppReferenceNumber
-		,se.ApplicationReferenceNumber				/*** SN005: Added ***/
-		,Decision_Date					= NULL /*** SN003: Commented out value causes confusion ***/ --se.[created] 
-		,Regulator_Status = Case when se.[Decision] is null then 'Pending'
-								else se.[Decision]
-							END
-		,Regulator_Rejection_Comments	= NULL /*** SN003: Commented out value causes confusion ***/ --se.[Comments] 
-		,RejectionComments				= ''  --not represented in Cosmos DB
-		,se.[Type]
-		,se.[UserId]
-		,rsd.Created								/*** SN002: Added 501408 -- SubmissionDate ***/
-		,RegistrationType				= 2			/*** SN002: Added 501408 - To allow logic in PowerBI Regulator_Status to be set to NULL ***/
-		,se.registrationreferencenumber
-	From
-		rpd.cosmos_file_metadata	cfm
-	Left Join
-		rpd.SubmissionEvents		se
-			on cfm.SubmissionId = se.SubmissionId
-	Left Join
-		RegSubDate					rsd
-			on se.SubmissionId = rsd.SubmissionId And rsd.RowNo=1
-Where
-se.[type] in ( 'RegistrationApplicationSubmitted') And se.ApplicationReferenceNumber is not null
+		From rpd.cosmos_file_metadata cfm
+		inner join rpd.Submissions s on s.SubmissionId = cfm.SubmissionId
+		left Join
+			SubmissionEvents_updated		se
+				on cfm.FileId = ISNULL(se.FileId,se.fileid_new) 
+					and se.[type] in ('RegulatorPoMDecision', 'RegulatorRegistrationDecision')
+		left join resubmission_ids rid on cfm.fileid = rid.fileid 
+		left join submitted_Fileids sfs on sfs.submitted_Fileid = cfm.fileid
+		left join top_matching_og_get_all_RegistrationApplicationSubmitted app_submitted on app_submitted.app_submitted_Fileid = cfm.fileid
+		Left Join [rpd].[Users] u on se.[Userid] = u.[userid] and u.[isdeleted] = 0
+		Left Join rpd.[persons] p on u.[id] =p.[userid] and p.[isdeleted] = 0
+		where cfm.FileType in ('CompanyDetails','Pom')
+),
 
+rank_on_res as
+(
+select  
+	 *
+	 ,row_number() over(partition by cfm_FileId order by Decision_Date desc, Created desc) as RowNumber
+from res
+),
+
+cd_pom_result as 
+(
+	select * From rank_on_res where RowNumber = 1
 )
-,
-Reg_set as
-(
-select distinct RegistrationSetId, Fileid
-from [rpd].[SubmissionEvents]
-where RegistrationSetId is not null and Fileid is not null
-),
-UploadedRegFiles as
-(
-	select rs.RegistrationSetId, ses.FileId, max(ses.Created) as uploaded_ts
-	from [rpd].[Submissions] s
-	inner join [rpd].[SubmissionEvents] ses on ses.SubmissionId = s.SubmissionId
-	left join Reg_set rs on rs.Fileid = ses.FileId
-	where s.SubmissionType = 'Registration'
-	and reverse(substring(reverse(trim(s.SubmissionPeriod)),1,2)) in ('25','26','27','28')
-	and ses.[Type] in ('Submitted')
-	group by  rs.RegistrationSetId, ses.FileId
-),
-se_with_reg as
-(
-	select distinct se.*, Reg_set.RegistrationSetId
-	from se
-	left join Reg_set on Reg_set.fileid = se.fileid
-),
-submitted_file_status AS (
-SELECT distinct
-		 c.[SubmissionId]
-		,c.[RegistrationSetId]
-		,c.[OrganisationId]	
-		,c.[FileName]
-		,c.[FileType]
-		,c.[OriginalFileName]
-		,c.[TargetDirectoryName]
-		,case when se.Regulator_Status is NULL and ugf.FileId is not NULL then ugf.uploaded_ts else se.Decision_Date end as Decision_Date
-		,case when se.Regulator_Status is NULL and ugf.FileId is not NULL then 'Uploaded' else se.Regulator_Status end as Regulator_Status
-		,'' AS [RegulatorDecision] --not represented in Cosmos DB
-		,Regulator_User_Name = Case When RegistrationType=2 and Regulator_Status='Pending' Then NULL Else ISNULL(p.[FirstName],'') +' '+ ISNULL(p.[LastName],'') End
-		,Regulator_Rejection_Comments = se.Regulator_Rejection_Comments
-		,'' AS [RejectionComments] --not represented in Cosmos DB
-		,se.[type]
-		,se.[UserId]
-		,Row_Number() Over(Partition by c.[filename] order by se.Decision_Date desc) as RowNumber
-		,se.Created
-		,se.RegistrationType
-		,c.SubmissionPeriod
-		,ApplicationReferenceNo		= Coalesce(se.AppReferenceNumber,se.ApplicationReferenceNumber,Null)		/*** SN005: Added ***/
-		,se.registrationreferencenumber
-FROM [rpd].[cosmos_file_metadata] c
-  Left Join se on se.FileId = c.FileId 
-  				/*** SN:001 Removed: and se.[type] in ('RegulatorPoMDecision', 'RegulatorRegistrationDecision') ***/
-  Left Join [rpd].[Users] u on se.[Userid] = u.[userid] and u.[isdeleted] = 0
-  Left Join rpd.[persons] p on u.[id] =p.[userid] and p.[isdeleted] = 0
-  left join UploadedRegFiles ugf on ugf.RegistrationSetId = c.RegistrationSetId
-  where c.FileType = 'Pom'
 
-  union
-
-SELECT distinct
-		 c.[SubmissionId]
-		,c.[RegistrationSetId]
-		,c.[OrganisationId]	
-		,c.[FileName]
-		,c.[FileType]
-		,c.[OriginalFileName]
-		,c.[TargetDirectoryName]
-		,case when se.Regulator_Status is NULL and ugf.FileId is not NULL then ugf.uploaded_ts else se.Decision_Date end as Decision_Date
-		,case when se.Regulator_Status is NULL and ugf.FileId is not NULL then 'Uploaded' else se.Regulator_Status end as Regulator_Status
-		,'' AS [RegulatorDecision] --not represented in Cosmos DB
-		,Regulator_User_Name = Case When RegistrationType=2 and Regulator_Status='Pending' Then NULL Else ISNULL(p.[FirstName],'') +' '+ ISNULL(p.[LastName],'') End
-		,Regulator_Rejection_Comments = se.Regulator_Rejection_Comments
-		,'' AS [RejectionComments] --not represented in Cosmos DB
-		,se.[type]
-		,se.[UserId]
-		,Row_Number() Over(Partition by c.[filename] order by se.Decision_Date desc) as RowNumber
-		,se.Created
-		,se.RegistrationType
-		,c.SubmissionPeriod
-		,ApplicationReferenceNo		= Coalesce(se.AppReferenceNumber,se.ApplicationReferenceNumber,Null)		/*** SN005: Added ***/
-		,se.registrationreferencenumber
-FROM [rpd].[cosmos_file_metadata] c
-  Left join se_with_reg se on se.RegistrationSetId = c.RegistrationSetId
-  				/*** SN:001 Removed: and se.[type] in ('RegulatorPoMDecision', 'RegulatorRegistrationDecision') ***/
-  Left Join [rpd].[Users] u on se.[Userid] = u.[userid] and u.[isdeleted] = 0
-  Left Join rpd.[persons] p on u.[id] =p.[userid] and p.[isdeleted] = 0
-  left join UploadedRegFiles ugf on ugf.RegistrationSetId = c.RegistrationSetId
-  where c.FileType <> 'Pom'
-
-) 
-select distinct sfs.SubmissionId
-		,sfs.[RegistrationSetId]
-		,sfs.[OrganisationId]
-		,sfs.[FileName]
-		,sfs.[FileType]
-		,sfs.[OriginalFileName]
-		,sfs.[TargetDirectoryName]
-		,sfs.[Decision_Date]
-		,sfs.[Regulator_Status]
-		,sfs.[RegulatorDecision]
-		,sfs.[Regulator_User_Name]
-		,sfs.[Regulator_Rejection_Comments]
-		,sfs.[RejectionComments]
-		,sfs.[type]
-		,sfs.[UserId]
-		,sfs.[RowNumber]
-		,sfs.Created
-		,sfs.RegistrationType
-		,sfs.SubmissionPeriod
-		,sfs.ApplicationReferenceNo			/*** SN005: Added ***/
-		,sfs.registrationreferencenumber
- from submitted_file_status sfs
-where sfs.[RowNumber] = 1;
+select distinct pom_cd.SubmissionId, pom_cd.RegistrationSetId, pom_cd.OrganisationId, pom_cd.FileName, pom_cd.FileType, pom_cd.OriginalFileName, pom_cd.TargetDirectoryName, pom_cd.Decision_Date, pom_cd.Regulator_Status, pom_cd.RegulatorDecision, pom_cd.Regulator_User_Name, pom_cd.Regulator_Rejection_Comments, pom_cd.RejectionComments, pom_cd.Type, pom_cd.UserId, pom_cd.RowNumber, pom_cd.Created, pom_cd.RegistrationType, pom_cd.SubmissionPeriod, pom_cd.ApplicationReferenceNo, pom_cd.registrationreferencenumber, pom_cd.Original_Regulator_Status, pom_cd.SubmissionType, pom_cd.IsResubmission_identifier, pom_cd.cfm_FileId, pom_cd.FileId, pom_cd.fileid_new, pom_cd.submitted_Fileid, pom_cd.SubmissionEventId_of_submitted_record, pom_cd.app_submitted_Fileid, pom_cd.SubmissionEventId_of_application_submitted_record
+from cd_pom_result pom_cd
+union
+select distinct cfm.SubmissionId, cfm.RegistrationSetId, cfm.OrganisationId, cfm.FileName, cfm.FileType, cfm.OriginalFileName, cfm.TargetDirectoryName, cp.Decision_Date, cp.Regulator_Status, cp.RegulatorDecision, cp.Regulator_User_Name, cp.Regulator_Rejection_Comments, cp.RejectionComments, cp.Type, cp.UserId, cp.RowNumber, cp.Created, cp.RegistrationType, cp.SubmissionPeriod, cp.ApplicationReferenceNo, cp.registrationreferencenumber, cp.Original_Regulator_Status, cp.SubmissionType, cp.IsResubmission_identifier, cp.cfm_FileId, cp.FileId, cp.fileid_new, cp.submitted_Fileid, cp.SubmissionEventId_of_submitted_record, cp.app_submitted_Fileid, cp.SubmissionEventId_of_application_submitted_record
+From rpd.cosmos_file_metadata cfm
+inner join cd_pom_result cp on cfm.RegistrationSetId = cp.RegistrationSetId
+where cfm.FileType in ('Partnerships','Brands');
