8256fcc89084f48207c412ce54d1d2bed7632a30
Sebastian Taylor <sebastian.taylor@eviden.com>
2025-04-25 15:52:11 +0100

    Ticket - 520218 - fix v_organisation_details_not_submitted (#269) (#278)
    Co-authored-by: Tufan Sahin <156186517+tufanshn@users.noreply.github.com>

--- dbo/Views/v_organisation_details_not_submitted.sql
+++ dbo/Views/v_organisation_details_not_submitted.sql
@@ -4,6 +4,8 @@ AS WITH
 	History:
 
 	Created: 2025-03-17:	YM001:	Ticket - 520218:	This view used for the Org Details - Not Submitted report
+	Update: 2025-04-16:		ST002:	Ticket - 520218: Fix to logic so that if an Org file has not been submitted it should show in this view
+														If an Org file HAS been submitted, it should NOT show on this view. Irespective of Registration file uploaded/status
 
 ******************************************************************************************************************************/
 
@@ -28,24 +30,17 @@ EnrolmentData AS (
         a.Regulator_Rejection_Comments
     FROM [dbo].[t_rpd_data_SECURITY_FIX] a -- Enrolment
 )
+
 SELECT 
     ed.*
 FROM EnrolmentData ed
 WHERE YEAR(ed.[Enrolment_CreatedOn]) < 2025
-and NOT EXISTS (
+--Only <2025 Enrolled organisations
+AND NOT EXISTS (
+--Exclude any record that has a confirmed org file submission
     SELECT distinct b.organisation_id
     FROM [rpd].[CompanyDetails] b
     INNER JOIN [dbo].[t_BrndPrtnr_Org_Submissions] rbp
         ON b.organisation_id = rbp.FromOrganisation_ReferenceNumber
     WHERE b.organisation_id = ed.FromOrganisation_ReferenceNumber
-)
-UNION 
-SELECT 
-    ed.*
-FROM EnrolmentData ed
-WHERE NOT EXISTS (
-    SELECT distinct b.organisation_id
-    FROM [rpd].[CompanyDetails] b
-    WHERE ed.FromOrganisation_ReferenceNumber = b.organisation_id
-)
-AND YEAR(ed.[Enrolment_CreatedOn]) < 2025;
+);
