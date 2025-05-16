8146f43a49323a1563f2cc1ad0c9efb1ac5d3091
Prasanna Thakku Mani <prasanna.thakkumani@eviden.com>
2025-05-16 07:40:45 +0100

    515337 master script

---
+++ dbo/Views/v_extract_recent_pom_org_large_data.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[v_extract_recent_pom_org_large_data] AS select *
+from dbo.t_extract_recent_pom_org_data a
+where not exists 
+(
+	select 1
+	from dbo.v_extract_recent_pom_org_small_data b
+	where a.org_id = b.org_id
+	and a.Reporting_Year=b.Reporting_Year
+);
