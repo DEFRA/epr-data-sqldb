c05cf643cd91be563bc5fea41540eeed7b91e863
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-06-13 17:15:09 +0100

    562694 master script v3 (#316)

--- dbo/Stored Procedures/sp_Extract_recent_pom_org_data_table_create.sql
+++ dbo/Stored Procedures/sp_Extract_recent_pom_org_data_table_create.sql
@@ -13,6 +13,51 @@ set @p_start_dt = getdate()
 INSERT INTO [dbo].[batch_log] ([ID],[ProcessName],[SubProcessName],[Count],[start_time_stamp],[end_time_stamp],[Comments],batch_id)
 select (select ISNULL(max(id),1)+1 from [dbo].[batch_log]),'sp_Extract_recent_pom_org_data_table_create','procedure', NULL, @p_start_dt, getdate(), 'Started',@batch_id
 
+--Table 1
+set @start_dt = getdate()
+
+		IF OBJECT_ID('dbo.t_rptRegistrationRegistered', 'U') IS NOT NULL
+		BEGIN
+			DROP TABLE dbo.t_rptRegistrationRegistered;
+		END;	
+ 
+		with rptRegistrationRegistered as
+		(
+			select distinct organisation_id, 'Y' as Is_Present_in_Reg_report
+			from [dbo].[registration]
+		)
+		SELECT *
+		INTO dbo.t_rptRegistrationRegistered
+		FROM rptRegistrationRegistered
+
+INSERT INTO [dbo].[batch_log] ([ID],[ProcessName],[SubProcessName],[Count],[start_time_stamp],[end_time_stamp],[Comments],batch_id)
+select (select ISNULL(max(id),1)+1 from [dbo].[batch_log]),'sp_Extract_recent_pom_org_data_table_create','t_rptRegistrationRegistered', NULL, @start_dt, getdate(), 'Completed',@batch_id
+
+
+--Table 2
+set @start_dt = getdate()
+
+		IF OBJECT_ID('dbo.t_rptPOM_All_Submissions', 'U') IS NOT NULL
+		BEGIN
+			DROP TABLE dbo.t_rptPOM_All_Submissions;
+		END;	
+ 
+		with rptPOM_All_Submissions as
+		(
+			select distinct OrganisationID as organisation_id, 'Y' as Is_Present_in_POM_report
+			from [dbo].[t_POM_All_Submissions]
+			where OrganisationID is not null
+		)
+		SELECT *
+		INTO dbo.t_rptPOM_All_Submissions
+		FROM rptPOM_All_Submissions
+
+INSERT INTO [dbo].[batch_log] ([ID],[ProcessName],[SubProcessName],[Count],[start_time_stamp],[end_time_stamp],[Comments],batch_id)
+select (select ISNULL(max(id),1)+1 from [dbo].[batch_log]),'sp_Extract_recent_pom_org_data_table_create','t_rptPOM_All_Submissions', NULL, @start_dt, getdate(), 'Completed',@batch_id
+
+
+
+--Table 3
 set @start_dt = getdate()
 
 		IF OBJECT_ID('dbo.t_extract_recent_pom_org_data', 'U') IS NOT NULL
---
+++ dbo/Tables/t_rptRegistrationRegistered.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[t_rptRegistrationRegistered] (
+    [organisation_id]          INT         NULL,
+    [Is_Present_in_Reg_report] VARCHAR (1) NOT NULL
+)
+WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
