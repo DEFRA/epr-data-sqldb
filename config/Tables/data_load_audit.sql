a4962662e63d54f1a6ed49e267d2a9afe31397f1
Sebastian Taylor <sebastian.taylor@eviden.com>
2025-06-16 09:23:43 +0100

    562039-Submissions-and-SubmissionEvents table updates (#317)

---
+++ config/Tables/data_load_audit.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [config].[data_load_audit] (
+    [table_name]    NVARCHAR (50) NULL,
+    [full_load_ind] NVARCHAR (1)  DEFAULT ('N') NULL
+)
+WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
