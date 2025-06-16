7dd31e3a061b499b3006921786b2e5f13ce1d053
Sebastian Taylor <sebastian.taylor@eviden.com>
2025-06-16 09:16:50 +0100

    562039-Submissions-and-SubmissionEvents table updates

--- rpd/Tables/SubmissionEvents.sql
+++ rpd/Tables/SubmissionEvents.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [rpd].[SubmissionEvents] (
     [IsPackagingResubmissionFeeViewed]     BIT             NULL,
     [PaidAmount]                           NVARCHAR (4000) NULL,
-    [OrganisationMembers]                  INT             NULL,
     [DecisionDate]                         NVARCHAR (4000) NULL,
     [RequiresRowValidation]                BIT             NULL,
     [IsResubmitted]                        BIT             NULL,
@@ -16,9 +15,8 @@
     [RegistrationReferenceNumber]          NVARCHAR (4000) NULL,
     [Comments]                             NVARCHAR (4000) NULL,
     [RegistrationSetId]                    NVARCHAR (4000) NULL,
-    [IsResubmissionRequired]               NVARCHAR (4000) NULL,
+    [IsResubmissionRequired]               BIT             NULL,
     [AppReferenceNumber]                   NVARCHAR (4000) NULL,
-    [DataSourceType]                       NVARCHAR (4000) NULL,
     [ApplicationReferenceNumber]           NVARCHAR (4000) NULL,
     [SubmissionDate]                       NVARCHAR (4000) NULL,
     [SubmissionEventId]                    NVARCHAR (4000) NULL,
@@ -29,13 +27,11 @@
     [HasMaxRowErrors]                      BIT             NULL,
     [RequiresValidation]                   BIT             NULL,
     [ContentScan]                          NVARCHAR (4000) NULL,
-    [CompanyDetailsFileId]                 NVARCHAR (4000) NULL,
     [SubmissionId]                         NVARCHAR (4000) NULL,
     [Decision]                             NVARCHAR (4000) NULL,
     [RegulatorDecision]                    NVARCHAR (4000) NULL,
     [PackagingResubmissionReferenceNumber] NVARCHAR (4000) NULL,
     [FileId]                               NVARCHAR (4000) NULL,
-    [RejectionComments]                    NVARCHAR (4000) NULL,
     [IsValid]                              BIT             NULL,
     [BlobName]                             NVARCHAR (4000) NULL,
     [AntivirusScanResult]                  NVARCHAR (4000) NULL,
@@ -44,13 +40,10 @@
     [Errors]                               NVARCHAR (4000) NULL,
     [FileName]                             NVARCHAR (4000) NULL,
     [AntivirusScanTrigger]                 NVARCHAR (4000) NULL,
-    [ResubmissionRequired]                 NVARCHAR (4000) NULL,
     [FileType]                             NVARCHAR (4000) NULL,
     [UserId]                               NVARCHAR (4000) NULL,
     [ProducerId]                           NVARCHAR (4000) NULL,
     [SubmittedBy]                          NVARCHAR (4000) NULL,
-    [HasWarnings]                          BIT             NULL,
-    [OrganisationMembersCount]             INT             NULL,
     [RegulatorUserId]                      NVARCHAR (4000) NULL,
     [PaymentMethod]                        NVARCHAR (4000) NULL,
     [IsResubmission]                       BIT             NULL,
@@ -58,5 +51,5 @@
     [BlobContainerName]                    NVARCHAR (4000) NULL,
     [load_ts]                              DATETIME2 (7)   NULL
 )
-WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = HASH([SubmissionEventId]));
 
