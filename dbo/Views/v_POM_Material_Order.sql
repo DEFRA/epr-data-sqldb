8c3507611dfc1287489197ab869a6052d7fb0001
Rakesh Mamidala <156447227+rakeshmamidala221@users.noreply.github.com>
2025-06-25 16:45:54 +0100

    Pom_ram_rag_rating&PRN (#329)

--- dbo/Views/v_POM_Material_Order.sql
+++ dbo/Views/v_POM_Material_Order.sql
@@ -1,9 +1,10 @@
 ﻿CREATE VIEW [dbo].[v_POM_Material_Order]
 AS SELECT distinct
-
-p.packaging_material as packaging_material_code
-
+p.packaging_material as packaging_material_code,
+p.packaging_material_subtype as packaging_material_subtype
 ,case 
+	when p.packaging_material = 'PL' AND p.packaging_material_subtype = 'Rigid' then 'Plastic-Rigid'
+	when p.packaging_material = 'PL' AND p.packaging_material_subtype = 'Flexible' then 'Plastic-Flexible'
     when p.packaging_material = 'AL' then 'Aluminium'
     when p.packaging_material = 'FC' then 'Fibre Composite'
     when p.packaging_material = 'GL' then 'Glass'
@@ -16,14 +17,17 @@ p.packaging_material as packaging_material_code
 end packaging_material
 
 ,case 
+	when p.packaging_material = 'PL' AND p.packaging_material_subtype = 'Flexible' then 2
+	when p.packaging_material = 'PL' AND p.packaging_material_subtype = 'Rigid' then 3
     when p.packaging_material = 'PL' then 1
-    when p.packaging_material = 'WD' then 2
-    when p.packaging_material = 'AL' then 3
-    when p.packaging_material = 'ST' then 4
-    when p.packaging_material = 'GL' then 5
-    when p.packaging_material = 'PC' then 6
-    when p.packaging_material = 'FC' then 7
-    when p.packaging_material = 'OT' then 8
+    when p.packaging_material = 'WD' then 4
+    when p.packaging_material = 'AL' then 5
+    when p.packaging_material = 'ST' then 6
+    when p.packaging_material = 'GL' then 7
+    when p.packaging_material = 'PC' then 8
+    when p.packaging_material = 'FC' then 9
+    when p.packaging_material = 'OT' then 10
+	
 -- else p.packaging_material
 end packaging_material_order
 from rpd.Pom p
