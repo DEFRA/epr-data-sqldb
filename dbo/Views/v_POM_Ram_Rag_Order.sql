bff8fa59b8ff6de6bfa47a7026e82c810b79a588
Sebastian Taylor <sebastian.taylor@eviden.com>
2025-06-26 13:39:50 +0100

    566012 check in of updated v_POM_Ram_Rag_Order to static script version

--- dbo/Views/v_POM_Ram_Rag_Order.sql
+++ dbo/Views/v_POM_Ram_Rag_Order.sql
@@ -1,17 +1,12 @@
 ﻿CREATE VIEW [dbo].[v_POM_Ram_Rag_Order]
-AS SELECT distinct
- 
-p.ram_rag_rating as ram_rag_rating_code
- 
-,case 
-    when p.ram_rag_rating = 'R' then 1
-    when p.ram_rag_rating = 'R-M' then 2
-    when p.ram_rag_rating = 'A' then 3
-    when p.ram_rag_rating = 'A-M' then 4
-    when p.ram_rag_rating = 'G' then 5
-    when p.ram_rag_rating = 'G-M' then 6
-end ram_rag_rating_order
-from rpd.Pom p
-where ram_rag_rating in (
-    'R','R-M','A','A-M','G','G-M'
-);
+AS SELECT 'R' ram_rag_rating_code, 1 ram_rag_rating_order
+UNION ALL
+SELECT 'R-M' ram_rag_rating_code, 2 ram_rag_rating_order
+UNION ALL
+SELECT 'A' ram_rag_rating_code, 3 ram_rag_rating_order
+UNION ALL
+SELECT 'A-M' ram_rag_rating_code, 4 ram_rag_rating_order
+UNION ALL
+SELECT 'G' ram_rag_rating_code, 5 ram_rag_rating_order
+UNION ALL
+SELECT 'G-M' ram_rag_rating_code, 6 ram_rag_rating_order;
