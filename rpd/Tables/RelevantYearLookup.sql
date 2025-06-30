8918473e515530c1079ff9ee8dcff533a9d75832
Sebastian Taylor <sebastian.taylor@eviden.com>
2025-06-30 10:57:00 +0100

    Code check in of [dbo].[v_Compliance_Report] - 563591 and (#333)
    [dbo].[v_CrossValidation_Landing_Page] 575965 and dependant objects into R13 for Jacob

---
+++ rpd/Tables/RelevantYearLookup.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [rpd].[RelevantYearLookup] (
+    [ID]              INT            NULL,
+    [Producer_Type]   NVARCHAR (100) NULL,
+    [Submission_Type] NVARCHAR (100) NULL,
+    [Start_Date]      DATE           NULL,
+    [End_Date]        DATE           NULL,
+    [Deadline_Date]   DATE           NULL,
+    [Relevant_Year]   INT            NULL,
+    [load_ts]         DATETIME2 (7)  NULL
+)
+WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
