bdc94cf009c5f82d6fb2c3373b186081f9158af3
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-07-03 10:23:42 +0100

    master_script_duplicate_data_removal (#345)

--- dbo/Views/v_reported_mandated_data_sets.sql
+++ dbo/Views/v_reported_mandated_data_sets.sql
@@ -1,4 +1,4 @@
-﻿CREATE VIEW [dbo].[v_reported_mandated_data_sets] AS select bs.[Org_ID] , sub_c.ReportingYear ,
+﻿CREATE VIEW [dbo].[v_reported_mandated_data_sets] AS select distinct bs.[Org_ID] , bs.Reporting_Year ,
 case when sub_c.cnt = 4 then 'Y' else 'N' end as Reported_mandated_data_sets
 from dbo.t_extract_recent_pom_org_data bs
 left join dbo.t_submission_count sub_c on sub_c.[Org ID] = bs.[Org_ID] and sub_c.ReportingYear = bs.Reporting_Year;
