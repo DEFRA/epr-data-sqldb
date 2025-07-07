a5201ef453bc0d45f7ae87cd781ca231ebd8ffe2
Tufan Sahin <156186517+tufanshn@users.noreply.github.com>
2025-07-07 14:12:48 +0100

    Profile Account Database and Define Correct Data Types for Delta Lake and Synapse Tables - Part 2 (#354)

--- rpd/Tables/InterOrganisationRoles.sql
+++ rpd/Tables/InterOrganisationRoles.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [rpd].[InterOrganisationRoles] (
-    [Id]      INT             NULL,
-    [Name]    NVARCHAR (4000) NULL,
-    [load_ts] DATETIME2 (7)   NULL
+    [Id]      INT            NULL,
+    [Name]    NVARCHAR (100) NULL,
+    [load_ts] DATETIME2 (7)  NULL
 )
 WITH (CLUSTERED INDEX([Name]), DISTRIBUTION = REPLICATE);
 
