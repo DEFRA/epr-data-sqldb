acb8cae77c19c470aaf7f2283a6479ed29726917
Prasanna Thakku Mani <159768350+PrasannaEviden@users.noreply.github.com>
2025-10-10 13:42:18 +0100

    624034_adding_3_new_tables (#415)

---
+++ rpd/Tables/CodeScenarioMapping.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [rpd].[CodeScenarioMapping] (
+    [Id]                  INT           NULL,
+    [CodeStatusConfigId]  INT           NULL,
+    [ScenarioReferenceId] INT           NULL,
+    [Active]              BIT           NULL,
+    [ExternalId]          NVARCHAR (36) NULL,
+    [CreatedOn]           NVARCHAR (50) NULL,
+    [LastUpdatedOn]       NVARCHAR (50) NULL,
+    [IsDeleted]           BIT           NULL,
+    [load_ts]             DATETIME2 (7) NULL
+)
+WITH (CLUSTERED INDEX([Id]), DISTRIBUTION = REPLICATE);
+
