19213ebd66ee63c1a89aefef33af792a8554ce87
Prasanna Thakku Mani <prasanna.thakkumani@eviden.com>
2025-10-10 13:36:17 +0100

    624034_adding_3_new_tables

---
+++ rpd/Tables/ScenarioReferences.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [rpd].[ScenarioReferences] (
+    [Id]             INT            NULL,
+    [ScenarioCode]   NVARCHAR (20)  NULL,
+    [Description]    NVARCHAR (255) NULL,
+    [ObligationFlag] NVARCHAR (50)  NULL,
+    [Active]         BIT            NULL,
+    [ExternalId]     NVARCHAR (36)  NULL,
+    [CreatedOn]      NVARCHAR (50)  NULL,
+    [LastUpdatedOn]  NVARCHAR (50)  NULL,
+    [IsDeleted]      BIT            NULL,
+    [load_ts]        DATETIME2 (7)  NULL
+)
+WITH (CLUSTERED INDEX([Id]), DISTRIBUTION = REPLICATE);
+
