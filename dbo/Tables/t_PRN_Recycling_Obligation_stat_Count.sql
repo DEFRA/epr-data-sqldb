f6750d9a99c5cd5059cb119fa47cd90d1d99f325
Sebastian Taylor <sebastian.taylor@atos.net>
2025-11-03 14:37:55 +0000

    Check in of 623983 Compliance Report Bug fix for Soft Deleted Obligation Calculations (#424)

---
+++ dbo/Tables/t_PRN_Recycling_Obligation_stat_Count.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[t_PRN_Recycling_Obligation_stat_Count] (
+    [ReportType]                         VARCHAR (24) NOT NULL,
+    [orgid]                              INT          NULL,
+    [Subsidiaryid]                       INT          NULL,
+    [YR]                                 INT          NULL,
+    [Recyling_Obligation]                INT          NULL,
+    [TOTAL PRN/PERN Awaiting Acceptance] INT          NULL,
+    [TOTAL PRN/PERN Accepted]            INT          NULL,
+    [TOTAl PRN/PERN Outstanding]         INT          NULL
+)
+WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
