4705f1981ac767ec824ce963163466651d25b178
Aravind Ravindran <112687857+AR-dataeng@users.noreply.github.com>
2025-11-28 15:18:21 +0000

    Update t_producer_obligation_determination.sql
    adding trading name and submission period year

--- dbo/Tables/t_producer_obligation_determination.sql
+++ dbo/Tables/t_producer_obligation_determination.sql
@@ -3,12 +3,14 @@
     [subsidiary_id]                NVARCHAR (6)    NULL,
     [submitter_id]                 NVARCHAR (4000) NOT NULL,
     [organisation_name]            NVARCHAR (4000) NOT NULL,
+    [trading_name]                 NVARCHAR (4000) NULL,
     [status_code]                  NVARCHAR (4000) NULL,
     [organisation_joining_date]    NVARCHAR (4000) NULL,
     [organisation_leaving_date]    NVARCHAR (4000) NULL,
     [obligation_status]            CHAR     (1)    NOT NULL,
     [calendar_year_days_obligated] SMALLINT        NULL,
-    [error_code]                   NVARCHAR (4000) NULL
+    [error_code]                   NVARCHAR (4000) NULL,
+    [submission_period_year]       INT             NULL
 )
 WITH(
     CLUSTERED COLUMNSTORE INDEX,
