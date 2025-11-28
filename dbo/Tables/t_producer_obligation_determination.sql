53ea0d3529e0d2af11d151d04bea449bd152e2f9
R Vadai <rvadai@segence.com>
2025-11-28 09:02:54 +0000

    rebased on release 17

---
+++ dbo/Tables/t_producer_obligation_determination.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[t_producer_obligation_determination] (
+    [organisation_id]              INT             NOT NULL,
+    [subsidiary_id]                NVARCHAR (6)    NULL,
+    [submitter_id]                 NVARCHAR (4000) NOT NULL,
+    [organisation_name]            NVARCHAR (4000) NOT NULL,
+    [status_code]                  NVARCHAR (4000) NULL,
+    [organisation_joining_date]    NVARCHAR (4000) NULL,
+    [organisation_leaving_date]    NVARCHAR (4000) NULL,
+    [obligation_status]            CHAR     (1)    NOT NULL,
+    [calendar_year_days_obligated] SMALLINT        NULL,
+    [error_code]                   NVARCHAR (4000) NULL
+)
+WITH(
+    CLUSTERED COLUMNSTORE INDEX,
+    DISTRIBUTION = HASH([organisation_id])
+);
