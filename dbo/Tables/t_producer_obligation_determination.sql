18cfddd49cf6121f65125f3285023e5b1096140b
Rob Vadai <rvadai@segence.com>
2025-11-28 16:12:27 +0000

    Adding t_producer_obligation_determination table (#433)
    * rebased on release 17
    
    * Update t_producer_obligation_determination.sql
    
    adding trading name and submission period year
    
    * Update t_producer_obligation_determination.sql
    
    * Update t_producer_obligation_determination.sql
    
    * Update t_producer_obligation_determination.sql
    
    ---------
    
    Co-authored-by: Aravind Ravindran <112687857+AR-dataeng@users.noreply.github.com>

---
+++ dbo/Tables/t_producer_obligation_determination.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[t_producer_obligation_determination] (
+    [organisation_id]              INT             NOT NULL,
+    [subsidiary_id]                NVARCHAR (6)    NULL,
+    [submitter_id]                 NVARCHAR (4000) NOT NULL,
+    [organisation_name]            NVARCHAR (4000) NOT NULL,
+    [trading_name]                 NVARCHAR (4000) NULL,
+    [status_code]                  NVARCHAR (4000) NULL,
+    [leaver_date]                  NVARCHAR (4000) NULL,
+    [joiner_date]                  NVARCHAR (4000) NULL,
+    [obligation_status]            CHAR    (1)    NOT NULL,
+    [num_days_obligated]           SMALLINT        NULL,
+    [error_code]                   NVARCHAR (4000) NULL,
+    [submission_period_year]       INT             NULL
+)
+WITH(
+    CLUSTERED COLUMNSTORE INDEX,
+    DISTRIBUTION = HASH([organisation_id])
+);
