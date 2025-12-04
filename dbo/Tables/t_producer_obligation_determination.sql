395659b8faa63c0e0672a1990fe064abf43e7d8a
Aravind Ravindran <112687857+AR-dataeng@users.noreply.github.com>
2025-12-04 16:28:12 +0000

    Update t_producer_obligation_determination.sql (#438)
    * Update t_producer_obligation_determination.sql
    
    * Update t_producer_obligation_determination.sql

--- dbo/Tables/t_producer_obligation_determination.sql
+++ dbo/Tables/t_producer_obligation_determination.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[t_producer_obligation_determination] (
-    [organisation_id]              INT             NOT NULL,
+    [organisation_id]              INT             NULL,
     [subsidiary_id]                NVARCHAR (4000) NULL,
-    [submitter_id]                 NVARCHAR (4000) NOT NULL,
-    [organisation_name]            NVARCHAR (4000) NOT NULL,
+    [submitter_id]                 NVARCHAR (4000) NULL,
+    [organisation_name]            NVARCHAR (4000) NULL,
     [trading_name]                 NVARCHAR (4000) NULL,
     [status_code]                  NVARCHAR (4000) NULL,
     [leaver_date]                  NVARCHAR (4000) NULL,
     [joiner_date]                  NVARCHAR (4000) NULL,
-    [obligation_status]            CHAR     (1)    NOT NULL,
+    [obligation_status]            CHAR     (1)    NULL,
     [num_days_obligated]           SMALLINT        NULL,
     [error_code]                   NVARCHAR (4000) NULL,
     [submission_period_year]       INT             NULL
