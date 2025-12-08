84a5ecc15a9a713c1b2ac59d676d395e9568094f
Prasanna Thakku Mani <prasanna.thakkumani@eviden.com>
2025-12-08 12:31:37 +0000

    SP to update stats and to rebuild indexes

---
+++ dbo/Stored Procedures/sp_gather_stats.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROC [dbo].[sp_gather_stats] AS
+begin
+--
+UPDATE STATISTICS rpd.organisations;
+UPDATE STATISTICS rpd.pom;
+UPDATE STATISTICS rpd.companydetails;
+UPDATE STATISTICS rpd.cosmos_file_metadata;
+UPDATE STATISTICS rpd.enrolments;
+--
+ALTER INDEX ALL 
+ON rpd.organisations
+REBUILD;
+--
+ALTER INDEX ALL 
+ON rpd.pom
+REBUILD;
+--
+ALTER INDEX ALL 
+ON rpd.companydetails
+REBUILD;
+--
+end;
