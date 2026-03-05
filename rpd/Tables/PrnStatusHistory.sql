935397660479c0f079b30250d86baecef38704cc
dmbustillo <42774550+dmbustillo@users.noreply.github.com>
2026-03-05 08:44:29 +0000

    added ObligationYear to PrnStatusHistory
    added ObligationYear to PrnStatusHistory

--- rpd/Tables/PrnStatusHistory.sql
+++ rpd/Tables/PrnStatusHistory.sql
@@ -6,7 +6,7 @@
     [PrnStatusIdFk]           INT             NULL,
     [PrnIdFk]                 INT             NULL,
     [Comment]                 NVARCHAR (4000) NULL,
+    [ObligationYear]          NVARCHAR (4000) NULL,
     [load_ts]                 DATETIME2 (7)   NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
-
