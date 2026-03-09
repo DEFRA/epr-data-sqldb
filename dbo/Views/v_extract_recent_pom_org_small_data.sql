8b2c3ce9c0c538d83612297041d01fc80108ab84
Nicolai Negru <nicolai.negru@equalexperts.com>
2026-03-09 15:27:58 +0000

    move commas to lines' end

--- dbo/Views/v_extract_recent_pom_org_small_data.sql
+++ dbo/Views/v_extract_recent_pom_org_small_data.sql
@@ -19,67 +19,69 @@ With Small_producer_recent_pom_org as
 			(Reporting_Year=2025	and Organisation_data_submission_period='July to Dec 2025 - H2')
 		)
 )
-select so.Org_ID
-,Org_name
-,CH_number
-,Nation_of_enrolment
-,Enrolment_date_time
-,Enrolment_status
-,Nation_of_Compliance_Scheme_regulator
-,case when so.Reporting_Year = 2024 then 'Jan to Dec 2024' when so.Reporting_Year = 2025 then 'Jan to Dec 2025' end as Packaging_data_submission_period
-,Packaging_data_first_submission_datetime
-,Packaging_data_first_submitted_CS_or_Direct
-,Packaging_data_first_submitted_CS_Nation
-,Packaging_data_first_submission_status
-,Packaging_data_first_submission_organisation_size
-,Packaging_data_latest_submission_datetime
-,Packaging_data_latest_submitted_CS_or_Direct
-,Packaging_data_latest_submitted_CS_Nation
-,Packaging_data_latest_submission_status
-,Packaging_data_latest_submission_organisation_size
-,case when so.Reporting_Year = 2024 then 'Jan to Dec 2025' when so.Reporting_Year = 2025 then 'Jan to Dec 2026' end as Organisation_data_submission_period
-,Organisation_data_first_submission_datetime
-,Organisation_data_first_submitted_CS_or_Direct
-,Organisation_data_first_submitted_CS_Nation
-,Organisation_data_first_submission_status
-,Organisation_data_first_submission_organisation_size
-,Organisation_data_latest_submission_datetime
-,Organisation_data_latest_submitted_CS_or_Direct
-,Organisation_data_latest_submitted_CS_Nation
-,Organisation_data_latest_submission_status
-,Organisation_data_latest_submission_organisation_size
-,Organisation_exists_in_most_recent_packaging_data_submission
-,Organisation_exists_in_most_recent_organisation_data_submission
-,Organisation_visible_in_PowerBI_Packaging_reports
-,Organisation_visible_in_PowerBI_Orgdata_reports
-,Single_File_Submission_Packaging
-,Single_File_Submission_Orgdata
-,ds.Reported_mandated_data_sets
-,Organisation_soft_deleted
-,[Household drinks containers-Aluminium (Kg)]
-,[Household drinks containers-Aluminium (No.Units)]
-,[Household drinks containers-Fibre Composite (Kg)]
-,[Household drinks containers-Fibre Composite (No.Units)]
-,[Household drinks containers-Glass (Kg)]
-,[Household drinks containers-Glass (No.Units)]
-,[Household drinks containers-Other (Kg)]
-,[Household drinks containers-Other (No.Units)]
-,[Household drinks containers-Paper / Card (Kg)]
-,[Household drinks containers-Paper / Card (No.Units)]
-,[Household drinks containers-Plastic (Kg)]
-,[Household drinks containers-Plastic (No.Units)]
-,[Household drinks containers-Steel (Kg)]
-,[Household drinks containers-Steel (No.Units)]
-,[Household drinks containers-Wood (Kg)]
-,[Household drinks containers-Wood (No.Units)]
-,[Small organisation packaging - all-Aluminium]
-,[Small organisation packaging - all-Fibre Composite]
-,[Small organisation packaging - all-Glass]
-,[Small organisation packaging - all-Other]
-,[Small organisation packaging - all-Paper / Card]
-,[Small organisation packaging - all-Plastic]
-,[Small organisation packaging - all-Steel]
-,[Small organisation packaging - all-Wood]
-,so.Reporting_Year
+
+select
+    so.Org_ID,
+    Org_name,
+    CH_number,
+    Nation_of_enrolment,
+    Enrolment_date_time,
+    Enrolment_status,
+    Nation_of_Compliance_Scheme_regulator,
+    case when so.Reporting_Year = 2024 then 'Jan to Dec 2024' when so.Reporting_Year = 2025 then 'Jan to Dec 2025' end as Packaging_data_submission_period,
+    Packaging_data_first_submission_datetime,
+    Packaging_data_first_submitted_CS_or_Direct,
+    Packaging_data_first_submitted_CS_Nation,
+    Packaging_data_first_submission_status,
+    Packaging_data_first_submission_organisation_size,
+    Packaging_data_latest_submission_datetime,
+    Packaging_data_latest_submitted_CS_or_Direct,
+    Packaging_data_latest_submitted_CS_Nation,
+    Packaging_data_latest_submission_status,
+    Packaging_data_latest_submission_organisation_size,
+    case when so.Reporting_Year = 2024 then 'Jan to Dec 2025' when so.Reporting_Year = 2025 then 'Jan to Dec 2026' end as Organisation_data_submission_period,
+    Organisation_data_first_submission_datetime,
+    Organisation_data_first_submitted_CS_or_Direct,
+    Organisation_data_first_submitted_CS_Nation,
+    Organisation_data_first_submission_status,
+    Organisation_data_first_submission_organisation_size,
+    Organisation_data_latest_submission_datetime,
+    Organisation_data_latest_submitted_CS_or_Direct,
+    Organisation_data_latest_submitted_CS_Nation,
+    Organisation_data_latest_submission_status,
+    Organisation_data_latest_submission_organisation_size,
+    Organisation_exists_in_most_recent_packaging_data_submission,
+    Organisation_exists_in_most_recent_organisation_data_submission,
+    Organisation_visible_in_PowerBI_Packaging_reports,
+    Organisation_visible_in_PowerBI_Orgdata_reports,
+    Single_File_Submission_Packaging,
+    Single_File_Submission_Orgdata,
+    ds.Reported_mandated_data_sets,
+    Organisation_soft_deleted,
+    [Household drinks containers-Aluminium (Kg)],
+    [Household drinks containers-Aluminium (No.Units)],
+    [Household drinks containers-Fibre Composite (Kg)],
+    [Household drinks containers-Fibre Composite (No.Units)],
+    [Household drinks containers-Glass (Kg)],
+    [Household drinks containers-Glass (No.Units)],
+    [Household drinks containers-Other (Kg)],
+    [Household drinks containers-Other (No.Units)],
+    [Household drinks containers-Paper / Card (Kg)],
+    [Household drinks containers-Paper / Card (No.Units)],
+    [Household drinks containers-Plastic (Kg)],
+    [Household drinks containers-Plastic (No.Units)],
+    [Household drinks containers-Steel (Kg)],
+    [Household drinks containers-Steel (No.Units)],
+    [Household drinks containers-Wood (Kg)],
+    [Household drinks containers-Wood (No.Units)],
+    [Small organisation packaging - all-Aluminium],
+    [Small organisation packaging - all-Fibre Composite],
+    [Small organisation packaging - all-Glass],
+    [Small organisation packaging - all-Other],
+    [Small organisation packaging - all-Paper / Card],
+    [Small organisation packaging - all-Plastic],
+    [Small organisation packaging - all-Steel],
+    [Small organisation packaging - all-Wood],
+    so.Reporting_Year
 from Small_producer_recent_pom_org so
 left join dbo.v_reported_mandated_data_sets ds on  ds.[Org_ID] = so.[Org_ID] and ds.ReportingYear = so.Reporting_Year;
