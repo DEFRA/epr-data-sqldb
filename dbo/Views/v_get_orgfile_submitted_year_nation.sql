2b3c0bdf8636c36529cce1ec6d6f043f9ef7f0e5
DinoGauslin76 <dino.gauslin@esynergy.co.uk>
2026-05-18 12:58:55 +0100

    Year specific change
    Changes to specific year logic

--- dbo/Views/v_get_orgfile_submitted_year_nation.sql
+++ dbo/Views/v_get_orgfile_submitted_year_nation.sql
@@ -1,22 +1,37 @@
-﻿CREATE VIEW [dbo].[v_get_orgfile_submitted_year_nation] AS SELECT DISTINCT Relevant_Year, Organisation_Nation_Name, CS_Nation_name, Link_Column
+/****** Object:  View [dbo].[v_get_orgfile_submitted_year_nation]    Script Date: 14/05/2026 10:36:16 ******/
+SET ANSI_NULLS ON
+GO
 
-FROM
-(
-    SELECT
-        CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT) + 1 AS Relevant_Year,
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[v_get_orgfile_submitted_year_nation] AS
+
+SELECT DISTINCT 
+    CASE
+--  WHEN CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT) >= 2023 -- removed year reliance
+    WHEN CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT) >= YEAR(GETDATE())
+                    THEN CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT) + 1
+        ELSE CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT)     
+    END AS Relevant_Year,
+      Organisation_Nation_Name,
+    CS_Nation_name,
+    'Link Column' AS Link_Column
+
+FROM [dbo].[v_latest_pending_or_accepted_orgfile_by_year]
+
+UNION ALL
+
+SELECT  
+    CASE
+--        WHEN CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT) >= 2023 -- removed year reliance
+          WHEN CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT) >= YEAR(GETDATE())
+            THEN CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT) + 1
+        ELSE CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT)
+    END AS Relevant_Year,
         Organisation_Nation_Name,
-        CS_Nation_name,
-        'Link Column' AS Link_Column
-    FROM
-        [dbo].[v_latest_pending_or_accepted_orgfile_by_year]
-
-    UNION ALL
-
-    SELECT
-        CAST('20' + RIGHT(TRIM(SubmissionPeriod), 2) AS INT) + 1 AS Relevant_Year,
-        Organisation_Nation_Name,
-        CS_Nation_name,
-        'Link Column' AS Link_Column
-    FROM
-        [dbo].[v_latest_accepted_orgfile_by_year]
-) AS A;
+    CS_Nation_name,
+    'Link Column' AS Link_Column
+ 
+FROM [dbo].[v_latest_accepted_orgfile_by_year];
+GO
--- dbo/Views/v_latest_accepted_orgfile_by_year.sql
+++ dbo/Views/v_latest_accepted_orgfile_by_year.sql
@@ -1,111 +1,128 @@
-﻿CREATE VIEW [dbo].[v_latest_accepted_orgfile_by_year] AS 
+/****** Object:  View [dbo].[v_latest_accepted_orgfile_by_year]    
+Script Date: 15/05/2026 09:56:32 ******/
+SET ANSI_NULLS ON
+GO
 
-WITH base_data AS (
-    SELECT 
-        m.OrganisationId AS meta_OrganisationId,
-        m.SubmissionPeriod,
+SET QUOTED_IDENTIFIER ON
+GO
 
-        TRY_CAST('20' + RIGHT(TRIM(m.SubmissionPeriod), 2) AS INT) - 1 AS ReportingYear,
+CREATE VIEW [dbo].[v_latest_accepted_orgfile_by_year] 
+AS WITH 
+base_data
+AS (
+	SELECT m.OrganisationId AS meta_OrganisationId,
+		m.SubmissionPeriod,
+		--   Replaced Logic 
+		--case when m.SubmissionPeriod = 'January to December 2025'
+		--	then 2024
+		--else
+		--	'20' + Reverse(Substring(Reverse(TRIM(m.SubmissionPeriod)), 1, 2)) 
+		--end AS ReportingYear,
+CASE 
+	 WHEN m.SubmissionPeriod LIKE 'January to December %'
+     THEN CAST(RIGHT(m.SubmissionPeriod,4) AS INT) - 1
+     ELSE CAST(RIGHT(TRIM(m.SubmissionPeriod),2) AS INT) + 2000
+END AS ReportingYear,
+		CONVERT(DATETIME, Substring(m.Created, 1, 23)) AS Submission_time,
+		m.FileType,
+		m.filename AS meta_filename,
+		UPPER(TRIM(ISNULL(st.Regulator_Status, 'PENDING'))) AS Regulator_Status,
+		m.[RegistrationSetId],
+		m.[ComplianceSchemeId],
+		cs.Name AS ComplianceSchemeName,
+		cs.Id AS CS_id,
+		n.name as CS_Nation_name	
+	FROM rpd.cosmos_file_metadata m
+	INNER JOIN dbo.v_submitted_pom_org_file_status st
+		ON m.filename = st.FileName
+	LEFT JOIN rpd.ComplianceSchemes cs
+		ON cs.ExternalId = m.ComplianceSchemeId
+	LEFT JOIN [rpd].[Nations] n 
+		ON n.id = cs.Nationid   
+	WHERE UPPER(TRIM(ISNULL(Regulator_Status, ''))) in ('ACCEPTED','GRANTED')
+	),
 
-        CONVERT(DATETIME, SUBSTRING(m.Created, 1, 23)) AS Submission_time,
-        m.FileType,
-        m.filename AS meta_filename,
+latest_CompanyDetails
+AS (
+	SELECT *
+	FROM (
+		SELECT *,
+			Row_number() OVER (
+				PARTITION BY coalesce(ComplianceSchemeId, meta_OrganisationId),
+				ReportingYear ORDER BY Submission_time DESC
+				) AS cd_rn
+		FROM base_data
+		WHERE UPPER(FileType) = 'COMPANYDETAILS'
+		) A
+	WHERE cd_rn = 1
+	),
 
-        UPPER(TRIM(ISNULL(st.Regulator_Status, 'PENDING'))) AS Regulator_Status,
+cd_org_combined
+AS (
+	SELECT o.ReferenceNumber AS file_submitted_organisation_reference,
+		o.IsComplianceScheme AS file_submitted_organisation_IsComplianceScheme,
+		cd.CS_Nation_name,
+		cd.meta_OrganisationId,
+		cd.SubmissionPeriod,
+		cd.ReportingYear,
+		cd.Submission_time,
+		cd.FileType,
+		cd.meta_filename,
+		cd.Regulator_Status,
+		cd.ComplianceSchemeName,
+		cd.CS_id
+	FROM latest_CompanyDetails cd
+	LEFT JOIN rpd.organisations o
+		ON cd.meta_OrganisationId = o.ExternalId
+	),
 
-        m.[RegistrationSetId],
-        m.[ComplianceSchemeId],
-        cs.Name AS ComplianceSchemeName,
-        cs.Id AS CS_id,
-        n.name AS CS_Nation_name
-    FROM rpd.cosmos_file_metadata m
-    INNER JOIN dbo.v_submitted_pom_org_file_status st
-        ON m.filename = st.FileName
-    LEFT JOIN rpd.ComplianceSchemes cs
-        ON cs.ExternalId = m.ComplianceSchemeId
-    LEFT JOIN rpd.Nations n 
-        ON n.id = cs.Nationid
-    WHERE UPPER(TRIM(ISNULL(st.Regulator_Status, 'PENDING'))) 
-          IN ('ACCEPTED','GRANTED')
-),
-
-latest_CompanyDetails AS (
-    SELECT *
-    FROM (
-        SELECT *,
-            ROW_NUMBER() OVER (
-                PARTITION BY COALESCE(ComplianceSchemeId, meta_OrganisationId),
-                             ReportingYear
-                ORDER BY Submission_time DESC
-            ) AS cd_rn
-        FROM base_data
-        WHERE UPPER(FileType) = 'COMPANYDETAILS'
-    ) A
-    WHERE cd_rn = 1
-),
-
-cd_org_combined AS (
-    SELECT 
-        o.ReferenceNumber AS file_submitted_organisation_reference,
-        o.IsComplianceScheme AS file_submitted_organisation_IsComplianceScheme,
-        cd.CS_Nation_name,
-        cd.meta_OrganisationId,
-        cd.SubmissionPeriod,
-        cd.ReportingYear,
-        cd.Submission_time,
-        cd.FileType,
-        cd.meta_filename,
-        cd.Regulator_Status,
-        cd.ComplianceSchemeName,
-        cd.CS_id
-    FROM latest_CompanyDetails cd
-    LEFT JOIN rpd.organisations o
-        ON cd.meta_OrganisationId = o.ExternalId
-),
-
-res AS (
-    SELECT 
-        com.*,
-        cd.organisation_id,
-        cd.subsidiary_id,
-        cd.organisation_name,
-        cd.companies_house_number,
-        cd.organisation_size,
-        cd.registered_addr_line1,
-        cd.registered_addr_line2,
-        cd.registered_city,
-        cd.registered_addr_county,
-        cd.registered_addr_postcode,
-        cd.registered_addr_country,
-        cd.registered_addr_phone_number,
-        cd.approved_person_first_name,
-        cd.approved_person_last_name,
-        cd.approved_person_email,
-        cd.approved_person_phone_number,
-        cd.delegated_person_first_name,
-        cd.delegated_person_last_name,
-        cd.delegated_person_email,
-        cd.delegated_person_phone_number,
-        cd.primary_contact_person_first_name,
-        cd.primary_contact_person_last_name,
-        cd.primary_contact_person_email,
-        cd.primary_contact_person_phone_number,
-        n.name AS Organisation_Nation_Name,
-        org.NationId AS Organisation_Nation_Id,
-        ISNULL(TRIM(cd.leaver_code), '') AS leaver_code,
-        cd.leaver_date,
-        cd.Organisation_change_reason,
-        cd.joiner_date
-    FROM cd_org_combined com
-    LEFT JOIN rpd.CompanyDetails cd
-        ON com.meta_filename = cd.filename
-    LEFT JOIN rpd.Organisations o
-        ON o.ReferenceNumber = cd.subsidiary_id
-    LEFT JOIN rpd.Organisations org
-        ON org.ReferenceNumber = cd.organisation_id 
-    LEFT JOIN rpd.Nations n 
-        ON n.id = org.Nationid
-)
-
+res
+AS (
+	SELECT com.*,
+		cd.organisation_id,
+		cd.subsidiary_id,
+		cd.organisation_name,
+		cd.companies_house_number,
+		cd.organisation_size,
+		cd.registered_addr_line1,
+		cd.registered_addr_line2,
+		cd.registered_city,
+		cd.registered_addr_county,
+		cd.registered_addr_postcode,
+		cd.registered_addr_country,
+		cd.registered_addr_phone_number,
+		cd.approved_person_first_name,
+		cd.approved_person_last_name,
+		cd.approved_person_email,
+		cd.approved_person_phone_number,
+		cd.delegated_person_first_name,
+		cd.delegated_person_last_name,
+		cd.delegated_person_email,
+		cd.delegated_person_phone_number,
+		cd.primary_contact_person_first_name,
+		cd.primary_contact_person_last_name,
+		cd.primary_contact_person_email,
+		cd.primary_contact_person_phone_number,
+		n.name AS Organisation_Nation_Name, 
+		org.[NationId] AS Organisation_Nation_Id, 
+		isnull(trim(cd.leaver_code),'') as leaver_code,
+		cd.leaver_date,
+		cd.Organisation_change_reason,
+		cd.joiner_date
+	FROM cd_org_combined com
+	LEFT JOIN rpd.CompanyDetails cd
+		ON com.meta_filename = cd.filename
+	
+	LEFT JOIN rpd.Organisations o
+		ON o.ReferenceNumber = cd.subsidiary_id
+	LEFT JOIN rpd.Organisations org
+		ON org.ReferenceNumber = cd.organisation_id 
+	LEFT JOIN [rpd].[Nations] n 
+		ON n.id = org.Nationid 
+	
+	)
 SELECT *
 FROM res;
+GO
+
+
