8b82866453e70f7766bbc5072cb493f77ce6cd97
Simon Anderson <simonanderson@MacBook-Pro.local>
2026-06-30 16:52:35 +0100

    Fixing unique IDs to NVARCHARS

--- dbo/Tables/t_ProducerPayCalParameters_resub.sql
+++ dbo/Tables/t_ProducerPayCalParameters_resub.sql
@@ -3,8 +3,8 @@ CREATE TABLE [dbo].[t_ProducerPayCalParameters_resub]
     [OrganisationExternalId]        NVARCHAR(50)     NULL,      -- cfm.OrganisationId      VERIFY (rpd.cosmos_file_metadata)
     [OrganisationId]                INT              NULL,      -- cd.Organisation_Id      VERIFY (rpd.CompanyDetails)
     [FileName]                      NVARCHAR(500)    NULL,      -- cd.FileName             VERIFY (rpd.CompanyDetails)
-    [FileId]                        UNIQUEIDENTIFIER NULL,      -- cfm.FileId              VERIFY (rpd.cosmos_file_metadata)
-    [RegistrationSetId]             UNIQUEIDENTIFIER NULL,      -- cfm.RegistrationSetId   VERIFY (rpd.cosmos_file_metadata)
+    [FileId]                        NVARCHAR(500)    NULL,      -- cfm.FileId              VERIFY (rpd.cosmos_file_metadata)
+    [RegistrationSetId]             NVARCHAR(500)    NULL,      -- cfm.RegistrationSetId   VERIFY (rpd.cosmos_file_metadata)
     [IsOnlineMarketPlace]           BIT              NULL,      -- CAST(... AS BIT)        confirmed
     [OrganisationSize]              NVARCHAR(50)     NULL,      -- cd.Organisation_Size    VERIFY (rpd.CompanyDetails)
     [ProducerSize]                  NVARCHAR(50)     NULL,      -- CASE over Organisation_Size; same family, len >= 5  VERIFY
