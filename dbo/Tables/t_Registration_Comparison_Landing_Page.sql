8dc100cefc8fe802db6e662938a12a3205094e15
Simon Anderson <Simon.Anderson@defra.onmicrosoft.com>
2026-07-13 18:29:50 +0100

    Fixing schemas

--- dbo/Tables/t_Registration_Comparison_Landing_Page.sql
+++ dbo/Tables/t_Registration_Comparison_Landing_Page.sql
@@ -1,22 +1,26 @@
-﻿CREATE TABLE [dbo].[t_Registration_Comparison_Landing_Page] (
-    [CompanyOrgId]            INT             NULL,
-    [Organisation]            NVARCHAR (4000) NULL,
-    [CompanyOriginalFileName] NVARCHAR (4000) NULL,
-    [SubmissionPeriod]        NVARCHAR (4000) NULL,
-    [IsSubmitted]             BIT             NULL,
-    [FileCode]                NVARCHAR (4000) NULL,
-    [Regulator_Status]        NVARCHAR (4000) NULL,
-    [FileName]                NVARCHAR (4000) NULL,
-    [CompanyFileType]         NVARCHAR (4000) NULL,
-    [SubmittedBy]             NVARCHAR (4000) NOT NULL,
-    [SubmitterEmail]          NVARCHAR (4000) NULL,
-    [ServiceRoleType]         NVARCHAR (4000) NULL,
-    [SubmissionDateTime]      DATETIME        NULL,
-    [Compliance_Year]         VARCHAR (4)     NOT NULL,
-    [ComplianceSchemeName]    NVARCHAR (4000) NULL,
-    [CSORPD]                  VARCHAR (17)    NOT NULL,
-    [Nation]                  VARCHAR (1)     NOT NULL,
-    [CompanyRegID]            NVARCHAR (4000) NULL
+﻿CREATE TABLE [dbo].[t_Registration_Comparison_Landing_Page]
+(
+    [CompanyOrgId]             int            NULL,
+    [Organisation]             nvarchar(4000) NULL,
+    [CompanyOriginalFileName]  nvarchar(4000) NULL,
+    [SubmissionPeriod]         nvarchar(4000) NULL,
+    [IsSubmitted]              bit            NULL,
+    [FileCode]                 nvarchar(4000) NULL,
+    [Regulator_Status]         nvarchar(4000) NULL,
+    [FileName]                 nvarchar(4000) NULL,
+    [CompanyFileType]          nvarchar(4000) NULL,
+    [SubmittedBy]              nvarchar(4000) NOT NULL,
+    [SubmitterEmail]           nvarchar(4000) NULL,
+    [ServiceRoleType]          nvarchar(100)  NULL,
+    [SubmissionDateTime]       datetime       NULL,
+    [Compliance_Year]          varchar(4)     NOT NULL,
+    [ComplianceSchemeName]     nvarchar(4000) NULL,
+    [CSORPD]                   varchar(17)    NOT NULL,
+    [Nation]                   varchar(1)     NOT NULL,
+    [CompanyRegID]             nvarchar(4000) NULL
 )
-WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
-
+WITH
+(
+    CLUSTERED COLUMNSTORE INDEX,
+    DISTRIBUTION = ROUND_ROBIN
+);
