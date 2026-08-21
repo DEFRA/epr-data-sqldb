f77439dc531212032d76bde50a0864cf0d8d3f1d
Nicolai Negru <nicolai.negru@equalexperts.com>
2026-08-21 16:34:22 +0100

    fallback to load_ts

--- dbo/Stored Procedures/sp_delete_duplicate_rows.sql
+++ dbo/Stored Procedures/sp_delete_duplicate_rows.sql
@@ -53,20 +53,37 @@ BEGIN TRY
 WITH RowsToDelete AS (
   SELECT
     [SubmissionId], [FileId], [UserId], [BlobName], [BlobContainerName], [FileType], [Created], [OriginalFileName], [RegistrationSetId], [OrganisationId], [DataSourceType], [SubmissionPeriod],
-    [IsSubmitted], [SubmissionType], [ComplianceSchemeId], [TargetDirectoryName], [TargetContainerName], [SourceContainerName], [FileName],
-    [load_ts],
-    ROW_NUMBER() OVER (PARTITION BY
-      [SubmissionId], [FileId], [UserId], [BlobName], [BlobContainerName], [FileType], [Created], [OriginalFileName],
-      [RegistrationSetId], [OrganisationId], [DataSourceType], [SubmissionPeriod], [IsSubmitted], [SubmissionType],
-      [ComplianceSchemeId], [TargetDirectoryName], [TargetContainerName], [SourceContainerName], [FileName]
-      ORDER BY [load_ts] DESC) AS rn
+    [IsSubmitted], [SubmissionType], [ComplianceSchemeId], [TargetDirectoryName], [TargetContainerName], [SourceContainerName], [FileName], load_ts,
+    ROW_NUMBER() OVER (PARTITION BY SubmissionId, FileName, BlobName ORDER BY load_ts DESC) AS rn
   FROM
     rpd.cosmos_file_metadata
 )
 
-DELETE
-FROM RowsToDelete
-WHERE rn > 1;
+DELETE a
+
+FROM  rpd.cosmos_file_metadata a
+left JOIN RowsToDelete b
+  ON isnull(a.[SubmissionId],'') = isnull(b.[SubmissionId],'')
+  AND isnull(a.[FileId],'') = isnull(b.[FileId],'')
+  AND isnull(a.[UserId],'') = isnull(b.[UserId],'')
+  AND isnull(a.[BlobName],'') = isnull(b.[BlobName],'')
+  AND isnull(a.[BlobContainerName],'') = isnull(b.[BlobContainerName],'')
+  AND isnull(a.[FileType] ,'')= isnull(b.[FileType],'')
+  AND isnull(a.[Created],'') = isnull(b.[Created],'')
+  AND isnull(a.[OriginalFileName],'') = isnull(b.[OriginalFileName],'')
+  AND isnull(a.[RegistrationSetId],'') = isnull(b.[RegistrationSetId],'')
+  AND isnull(a.[OrganisationId] ,'')= isnull(b.[OrganisationId],'')
+  AND isnull(a.[DataSourceType],'') = isnull(b.[DataSourceType],'')
+  AND isnull(a.[SubmissionPeriod],'') = isnull(b.[SubmissionPeriod],'')
+  AND isnull(a.[IsSubmitted],'') = isnull(b.[IsSubmitted],'')
+  AND isnull(a.[SubmissionType],'') = isnull(b.[SubmissionType],'')
+  AND isnull(a.[ComplianceSchemeId],'') = isnull(b.[ComplianceSchemeId],'')
+  AND isnull(a.[TargetDirectoryName],'') = isnull(b.[TargetDirectoryName],'')
+  AND isnull(a.[TargetContainerName],'') = isnull(b.[TargetContainerName],'')
+  AND isnull(a.[SourceContainerName],'') = isnull(b.[SourceContainerName],'')
+  AND isnull(a.[FileName],'') = isnull(b.[FileName],'')
+  AND a.load_ts = b.load_ts
+WHERE b.rn > 1;
 
 
 
